6b7bf728613ae7346776ce466120e92c8443a723
Zach Carrera <zachcarrera@gmail.com>
2023-06-01 23:32:38 -0700

    add doc comments to package, variables, and functions so all the tests can pass

--- weather-forecast/weather_forecast.go
+++ weather-forecast/weather_forecast.go
@@ -1,8 +1,13 @@
+// Package weather provides tools to get information on the current weather.
 package weather
 
+// CurrentCondition represents a weather condition.
 var CurrentCondition string
+
+// CurrentLocation represents a location to check the weather.
 var CurrentLocation string
 
+// Forecast returns a string containing the weather forcast for a specific location.
 func Forecast(city, condition string) string {
 	CurrentLocation, CurrentCondition = city, condition
 	return CurrentLocation + " - current weather condition: " + CurrentCondition
