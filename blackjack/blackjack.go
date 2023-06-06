804e8569ea19984deb5777f597f77162b29ae5fd
Zach Carrera <zachcarrera@gmail.com>
2023-06-05 17:43:07 -0700

    blackjack: implement ParseCard

--- blackjack/blackjack.go
+++ blackjack/blackjack.go
@@ -2,11 +2,42 @@ package blackjack
 
 // ParseCard returns the integer value of a card following blackjack ruleset.
 func ParseCard(card string) int {
-	panic("Please implement the ParseCard function")
+	switch card {
+	case "ace":
+		return 11
+	case "two":
+		return 2
+	case "three":
+		return 3
+	case "four":
+		return 4
+	case "five":
+		return 5
+	case "six":
+		return 6
+	case "seven":
+		return 7
+	case "eight":
+		return 8
+	case "nine":
+		return 9
+	case "ten":
+		return 10
+	case "jack":
+		return 10
+	case "queen":
+		return 10
+	case "king":
+		return 10
+	default:
+		return 0
+
+	}
 }
 
 // FirstTurn returns the decision for the first turn, given two cards of the
 // player and one card of the dealer.
 func FirstTurn(card1, card2, dealerCard string) string {
+	return ""
 	panic("Please implement the FirstTurn function")
 }
