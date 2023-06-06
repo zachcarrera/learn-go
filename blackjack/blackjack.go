b7b16763073f1282b926d8194e7b812006068753
Zach Carrera <zachcarrera@gmail.com>
2023-06-05 18:23:07 -0700

    blackjack: refactor ParseCard and implement FirstTurn

--- blackjack/blackjack.go
+++ blackjack/blackjack.go
@@ -21,13 +21,7 @@ func ParseCard(card string) int {
 		return 8
 	case "nine":
 		return 9
-	case "ten":
-		return 10
-	case "jack":
-		return 10
-	case "queen":
-		return 10
-	case "king":
+	case "ten", "jack", "queen", "king":
 		return 10
 	default:
 		return 0
@@ -38,6 +32,24 @@ func ParseCard(card string) int {
 // FirstTurn returns the decision for the first turn, given two cards of the
 // player and one card of the dealer.
 func FirstTurn(card1, card2, dealerCard string) string {
-	return ""
-	panic("Please implement the FirstTurn function")
+
+	handValue := ParseCard(card1) + ParseCard(card2)
+	dealerValue := ParseCard(dealerCard)
+
+	switch {
+	case handValue == 22:
+		return "P"
+	case handValue == 21 && dealerValue < 10:
+		return "W"
+	case handValue == 21:
+		return "S"
+	case handValue >= 17 && handValue <= 20:
+		return "S"
+	case handValue >= 12 && handValue <= 16 && dealerValue >= 7:
+		return "H"
+	case handValue >= 12 && handValue <= 16:
+		return "S"
+	default:
+		return "H"
+	}
 }
