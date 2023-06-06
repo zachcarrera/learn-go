ef8c89b9a25372300dcba4a507084823d5b20db6
Zach Carrera <zachcarrera@gmail.com>
2023-06-05 22:31:22 -0700

    card-tricks: implement FavoriteCards and GetItem and add returns to other functions so the tests can build

--- card-tricks/card_tricks.go
+++ card-tricks/card_tricks.go
@@ -2,27 +2,30 @@ package cards
 
 // FavoriteCards returns a slice with the cards 2, 6 and 9 in that order.
 func FavoriteCards() []int {
-	panic("Please implement the FavoriteCards function")
+	return []int{2, 6, 9}
 }
 
 // GetItem retrieves an item from a slice at given position.
 // If the index is out of range, we want it to return -1.
 func GetItem(slice []int, index int) int {
-	panic("Please implement the GetItem function")
+	if index >= len(slice) || index < 0 {
+		return -1
+	}
+	return slice[index]
 }
 
 // SetItem writes an item to a slice at given position overwriting an existing value.
 // If the index is out of range the value needs to be appended.
 func SetItem(slice []int, index, value int) []int {
-	panic("Please implement the SetItem function")
+	return []int{2, 4}
 }
 
 // PrependItems adds an arbitrary number of values at the front of a slice.
 func PrependItems(slice []int, values ...int) []int {
-	panic("Please implement the PrependItems function")
+	return []int{2, 4}
 }
 
 // RemoveItem removes an item from a slice by modifying the existing slice.
 func RemoveItem(slice []int, index int) []int {
-	panic("Please implement the RemoveItem function")
+	return []int{2, 4}
 }
