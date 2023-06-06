71c7e758ac2b6186917ba2e36c471aa51bfc0322
Zach Carrera <zachcarrera@gmail.com>
2023-06-06 16:53:30 -0700

    animal-magic: replace all panics with return statements so the tests can build

--- animal-magic/animal_magic.go
+++ animal-magic/animal_magic.go
@@ -2,20 +2,20 @@ package chance
 
 // SeedWithTime seeds math/rand with the current computer time.
 func SeedWithTime() {
-	panic("Please implement the SeedWithTime function")
+	return
 }
 
 // RollADie returns a random int d with 1 <= d <= 20.
 func RollADie() int {
-	panic("Please implement the RollADie function")
+	return 0
 }
 
 // GenerateWandEnergy returns a random float64 f with 0.0 <= f < 12.0.
 func GenerateWandEnergy() float64 {
-	panic("Please implement the GenerateWandEnergy function")
+	return 0.0
 }
 
 // ShuffleAnimals returns a slice with all eight animal strings in random order.
 func ShuffleAnimals() []string {
-	panic("Please implement the ShuffleAnimals function")
+	return []string{"hello", "world"}
 }
