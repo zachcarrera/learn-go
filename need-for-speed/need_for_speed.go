8819b0a8e984faf61ed052acea1e6673e61da60d
Zach Carrera <zachcarrera@gmail.com>
2023-06-05 17:06:10 -0700

    need-for-speed: fix typo for Car.batteryDrain and implement Drive and CanFinish

--- need-for-speed/need_for_speed.go
+++ need-for-speed/need_for_speed.go
@@ -2,18 +2,18 @@ package speed
 
 // TODO: define the 'Car' type struct
 type Car struct {
-	battery     int
-	batterDrain int
-	speed       int
-	distance    int
+	battery      int
+	batteryDrain int
+	speed        int
+	distance     int
 }
 
 // NewCar creates a new remote controlled car with full battery and given specifications.
 func NewCar(speed, batteryDrain int) Car {
 	return Car{
-		speed:       speed,
-		batterDrain: batteryDrain,
-		battery:     100,
+		speed:        speed,
+		batteryDrain: batteryDrain,
+		battery:      100,
 	}
 }
 
@@ -32,12 +32,15 @@ func NewTrack(distance int) Track {
 // Drive drives the car one time. If there is not enough battery to drive one more time,
 // the car will not move.
 func Drive(car Car) Car {
-	return NewCar(4, 4)
-	panic("Please implement the Drive function")
+
+	if car.battery >= car.batteryDrain {
+		car.battery -= car.batteryDrain
+		car.distance += car.speed
+	}
+	return car
 }
 
 // CanFinish checks if a car is able to finish a certain track.
 func CanFinish(car Car, track Track) bool {
-	return false
-	panic("Please implement the CanFinish function")
+	return (track.distance/car.speed)*car.batteryDrain <= car.battery
 }
