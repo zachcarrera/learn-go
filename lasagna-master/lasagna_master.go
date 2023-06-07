b3dbf5045e7e53da29012a1ccf1b945e99b44bcb
Zach Carrera <zachcarrera@gmail.com>
2023-06-07 11:00:04 -0700

    lasagna-master: implement AddSecretIngredient

--- lasagna-master/lasagna_master.go
+++ lasagna-master/lasagna_master.go
@@ -23,6 +23,7 @@ func Quantities(layers []string) (int, float64) {
 
 // TODO: define the 'AddSecretIngredient()' function
 func AddSecretIngredient(friendsList []string, myList []string) {
+	myList[len(myList)-1] = friendsList[len(friendsList)-1]
 	return
 }
 
