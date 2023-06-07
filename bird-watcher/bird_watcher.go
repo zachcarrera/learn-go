7fd3f231c00e7a3125835bdf236697c784cb6829
Zach Carrera <zachcarrera@gmail.com>
2023-06-06 18:50:55 -0700

    bird-watcher: replace all panics with returns to the tests can build

--- bird-watcher/bird_watcher.go
+++ bird-watcher/bird_watcher.go
@@ -3,17 +3,17 @@ package birdwatcher
 // TotalBirdCount return the total bird count by summing
 // the individual day's counts.
 func TotalBirdCount(birdsPerDay []int) int {
-	panic("Please implement the TotalBirdCount() function")
+	return 0
 }
 
 // BirdsInWeek returns the total bird count by summing
 // only the items belonging to the given week.
 func BirdsInWeek(birdsPerDay []int, week int) int {
-	panic("Please implement the BirdsInWeek() function")
+	return 0
 }
 
 // FixBirdCountLog returns the bird counts after correcting
 // the bird counts for alternate days.
 func FixBirdCountLog(birdsPerDay []int) []int {
-	panic("Please implement the FixBirdCountLog() function")
+	return []int{5,3}
 }
