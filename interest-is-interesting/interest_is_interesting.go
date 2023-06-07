2213850383a64def390d69af0fa54a890c328b60
Zach Carrera <zachcarrera@gmail.com>
2023-06-06 22:08:18 -0700

    interest-is-interesting: implement InterestRate

--- interest-is-interesting/interest_is_interesting.go
+++ interest-is-interesting/interest_is_interesting.go
@@ -2,7 +2,16 @@ package interest
 
 // InterestRate returns the interest rate for the provided balance.
 func InterestRate(balance float64) float32 {
-	return 0.0
+	switch {
+	case balance < 0:
+		return 3.213
+	case balance < 1000:
+		return 0.5
+	case balance < 5000:
+		return 1.621
+	default:
+		return 2.475
+	}
 }
 
 // Interest calculates the interest for the provided balance.
