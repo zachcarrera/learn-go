9943a205b7381bf94d9894832350a6c490cc6e06
Zach Carrera <zachcarrera@gmail.com>
2023-06-06 22:04:05 -0700

    interest-is-interesting: add zero value returns so the tests can build

--- interest-is-interesting/interest_is_interesting.go
+++ interest-is-interesting/interest_is_interesting.go
@@ -2,20 +2,20 @@ package interest
 
 // InterestRate returns the interest rate for the provided balance.
 func InterestRate(balance float64) float32 {
-	panic("Please implement the InterestRate function")
+	return 0.0
 }
 
 // Interest calculates the interest for the provided balance.
 func Interest(balance float64) float64 {
-	panic("Please implement the Interest function")
+	return 0.0
 }
 
 // AnnualBalanceUpdate calculates the annual balance update, taking into account the interest rate.
 func AnnualBalanceUpdate(balance float64) float64 {
-	panic("Please implement the AnnualBalanceUpdate function")
+	return 0.0
 }
 
 // YearsBeforeDesiredBalance calculates the minimum number of years required to reach the desired balance.
 func YearsBeforeDesiredBalance(balance, targetBalance float64) int {
-	panic("Please implement the YearsBeforeDesiredBalance function")
+	return 0
 }
