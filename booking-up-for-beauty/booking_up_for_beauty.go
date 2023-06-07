3a65e2cb85b41135dcdf9c24bedbde199f3cd295
Zach Carrera <zachcarrera@gmail.com>
2023-06-07 14:40:37 -0700

    booking-up-for-beauty: implement IsAfternoonAppointment

--- booking-up-for-beauty/booking_up_for_beauty.go
+++ booking-up-for-beauty/booking_up_for_beauty.go
@@ -18,7 +18,9 @@ func HasPassed(date string) bool {
 
 // IsAfternoonAppointment returns whether a time is in the afternoon.
 func IsAfternoonAppointment(date string) bool {
-	panic("Please implement the IsAfternoonAppointment function")
+	layout := "Monday, January 2, 2006 15:04:05"
+	parsedTime, _ := time.Parse(layout, date)
+	return parsedTime.Hour() >= 12 && parsedTime.Hour() < 18
 }
 
 // Description returns a formatted string of the appointment time.
