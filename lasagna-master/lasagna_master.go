2c35d4f4591e9e94c566c813566a1a0ab750be4a
Zach Carrera <zachcarrera@gmail.com>
2023-06-07 10:23:03 -0700

    lasagna-master: create function signatures so that tests can compile

--- lasagna-master/lasagna_master.go
+++ lasagna-master/lasagna_master.go
@@ -1,18 +1,30 @@
 package lasagna
 
 // TODO: define the 'PreparationTime()' function
+func PreparationTime(layers []string, prepTimePerMinute int) int {
+	return 0
+}
 
 // TODO: define the 'Quantities()' function
+func Quantities(layers []string) (int, float64) {
+	return 0, 0.0
+}
 
 // TODO: define the 'AddSecretIngredient()' function
+func AddSecretIngredient(friendsList []string, myList []string) {
+	return
+}
 
 // TODO: define the 'ScaleRecipe()' function
+func ScaleRecipe(quantities []float64, portions int) []float64 {
+	return quantities
+}
 
 // Your first steps could be to read through the tasks, and create
 // these functions with their correct parameter lists and return types.
 // The function body only needs to contain `panic("")`.
-// 
+//
 // This will make the tests compile, but they will fail.
 // You can then implement the function logic one by one and see
-// an increasing number of tests passing as you implement more 
+// an increasing number of tests passing as you implement more
 // functionality.
