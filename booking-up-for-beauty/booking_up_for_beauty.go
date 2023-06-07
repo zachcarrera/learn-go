ba52f3a143df44cc77cba75870dc19f8e978d901
Zach Carrera <zachcarrera@gmail.com>
2023-06-07 13:01:22 -0700

    booking-up-for-beauty: implement HasPassed

--- booking-up-for-beauty/booking_up_for_beauty.go
+++ booking-up-for-beauty/booking_up_for_beauty.go
@@ -5,14 +5,15 @@ import "time"
 // Schedule returns a time.Time from a string containing a date.
 func Schedule(date string) time.Time {
 	layout := "1/2/2006 15:04:05"
-	time, _ := time.Parse(layout, date)
-	return time
-	panic("Please implement the Schedule function")
+	parsedTime, _ := time.Parse(layout, date)
+	return parsedTime
 }
 
 // HasPassed returns whether a date has passed.
 func HasPassed(date string) bool {
-	panic("Please implement the HasPassed function")
+	layout := "January 2, 2006 15:04:05"
+	parsedTime, _ := time.Parse(layout, date)
+	return time.Since(parsedTime) > 0
 }
 
 // IsAfternoonAppointment returns whether a time is in the afternoon.
