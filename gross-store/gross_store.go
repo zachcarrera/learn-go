0162ad697c7912f7bee7e211c22ce1625d1758c5
Zach Carrera <zachcarrera@gmail.com>
2023-06-07 18:07:42 -0700

    gross-store: implement Units

--- gross-store/gross_store.go
+++ gross-store/gross_store.go
@@ -2,7 +2,15 @@ package gross
 
 // Units stores the Gross Store unit measurements.
 func Units() map[string]int {
-	return map[string]int{}
+	units := map[string]int{
+		"quarter_of_a_dozen": 3,
+		"half_of_a_dozen":    6,
+		"dozen":              12,
+		"small_gross":        120,
+		"gross":              144,
+		"great_gross":        1728,
+	}
+	return units
 }
 
 // NewBill creates a new bill.
