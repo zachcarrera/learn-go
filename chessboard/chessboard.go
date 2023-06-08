c4ebbf63a4500e2ddcae9a6493fc01ce461eada4
Zach Carrera <zachcarrera@gmail.com>
2023-06-07 20:56:36 -0700

    chessboard: create type definitions for File and Chessboard

--- chessboard/chessboard.go
+++ chessboard/chessboard.go
@@ -1,8 +1,10 @@
 package chessboard
 
 // Declare a type named File which stores if a square is occupied by a piece - this will be a slice of bools
+type File []bool
 
 // Declare a type named Chessboard which contains a map of eight Files, accessed with keys from "A" to "H"
+type Chessboard map[string]File
 
 // CountInFile returns how many squares are occupied in the chessboard,
 // within the given file.
