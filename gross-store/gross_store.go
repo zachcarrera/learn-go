2bc64c53d9382b15a804c41925703b0be81c1985
Zach Carrera <zachcarrera@gmail.com>
2023-06-07 17:47:08 -0700

    gross-store: add zero value returns to all functions so the tests can build

--- gross-store/gross_store.go
+++ gross-store/gross_store.go
@@ -2,25 +2,25 @@ package gross
 
 // Units stores the Gross Store unit measurements.
 func Units() map[string]int {
-	panic("Please implement the Units() function")
+	return map[string]int{}
 }
 
 // NewBill creates a new bill.
 func NewBill() map[string]int {
-	panic("Please implement the NewBill() function")
+	return map[string]int{}
 }
 
 // AddItem adds an item to customer bill.
 func AddItem(bill, units map[string]int, item, unit string) bool {
-	panic("Please implement the AddItem() function")
+	return false
 }
 
 // RemoveItem removes an item from customer bill.
 func RemoveItem(bill, units map[string]int, item, unit string) bool {
-	panic("Please implement the RemoveItem() function")
+	return false
 }
 
 // GetItem returns the quantity of an item that the customer has in his/her bill.
 func GetItem(bill map[string]int, item string) (int, bool) {
-	panic("Please implement the GetItem() function")
+	return 0, false
 }
