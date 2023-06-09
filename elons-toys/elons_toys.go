f6a214c4a1c3b10666f7a5317e9301e2b14a6aae
Zach Carrera <zachcarrera@gmail.com>
2023-06-08 21:54:34 -0700

    elons-toys: create method signatures and add panic() in all of them so the tests can build

--- elons-toys/elons_toys.go
+++ elons-toys/elons_toys.go
@@ -1,18 +1,30 @@
 package elon
 
 // TODO: define the 'Drive()' method
+func (c *Car) Drive() {
+	panic("implement Drive")
+}
 
 // TODO: define the 'DisplayDistance() string' method
+func (c *Car) DisplayDistance() string {
+	panic("implement DisplayDistance")
+}
 
 // TODO: define the 'DisplayBattery() string' method
+func (c *Car) DisplayBattery() string {
+	panic("implement DisplayBattery")
+}
 
 // TODO: define the 'CanFinish(trackDistance int) bool' method
+func (c *Car) CanFinish(trackDistance int) bool {
+	panic("implement CanFinish")
+}
 
 // Your first steps could be to read through the tasks, and create
 // these functions with their correct parameter lists and return types.
 // The function body only needs to contain `panic("")`.
-// 
+//
 // This will make the tests compile, but they will fail.
 // You can then implement the function logic one by one and see
-// an increasing number of tests passing as you implement more 
+// an increasing number of tests passing as you implement more
 // functionality.
