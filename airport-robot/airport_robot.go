1d53f682817cdfc019bc60a3c4fc4157d2240bfc
Zach Carrera <zachcarrera@gmail.com>
2023-06-12 13:36:06 -0700

    airport-robot: implement SayHello

--- airport-robot/airport_robot.go
+++ airport-robot/airport_robot.go
@@ -1,9 +1,15 @@
 package airportrobot
 
+import "fmt"
+
 // Write your code here.
 // This exercise does not have tests for each individual task.
 // Try to solve all the tasks first before running the tests.
 type Greeter interface {
 	LanguageName() string
 	Greet(visitorName string) string
+}
+
+func SayHello(name string, greeter Greeter) string {
+	return fmt.Sprintf("I can speak %s: %s", greeter.LanguageName(), greeter.Greet(name))
 }
