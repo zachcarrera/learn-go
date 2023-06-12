9663bc250f11fbfdb0de77d91ed1cc76563dfcf1
Zach Carrera <zachcarrera@gmail.com>
2023-06-12 14:37:39 -0700

    meteorology: implement Stringer for TemperatureUnit

--- meteorology/meteorology.go
+++ meteorology/meteorology.go
@@ -8,6 +8,10 @@ const (
 )
 
 // Add a String method to the TemperatureUnit type
+func (t TemperatureUnit) String() string {
+	units := []string{"°C", "°F"}
+	return units[t]
+}
 
 type Temperature struct {
 	degree int
