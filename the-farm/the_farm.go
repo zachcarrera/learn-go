29cfdd881600cc0c78c5ead45d82e601f99bd438
Zach Carrera <zachcarrera@gmail.com>
2023-06-12 20:11:25 -0700

    sorting-room: implement step 4 and create a custom error type

--- the-farm/the_farm.go
+++ the-farm/the_farm.go
@@ -1,10 +1,21 @@
 package thefarm
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+)
 
 // See types.go for the types defined for this exercise.
 
 // TODO: Define the SillyNephewError type here.
+type SillyNephewError struct {
+	cows int
+}
+
+func (e *SillyNephewError) Error() string {
+	return fmt.Sprintf("silly nephew, there cannot be %d cows", e.cows)
+
+}
 
 // DivideFood computes the fodder amount per cow for the given cows.
 func DivideFood(weightFodder WeightFodder, cows int) (float64, error) {
@@ -26,6 +37,10 @@ func DivideFood(weightFodder WeightFodder, cows int) (float64, error) {
 
 	if amount < 0 {
 		return 0.0, errors.New("negative fodder")
+	}
+
+	if cows < 0 {
+		return 0.0, &SillyNephewError{cows: cows}
 	}
 
 	return amount / float64(cows), nil
