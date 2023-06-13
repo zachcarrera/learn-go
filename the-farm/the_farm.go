428f975c2e9716311f1415f993740e2b555f72f2
Zach Carrera <zachcarrera@gmail.com>
2023-06-12 17:38:44 -0700

    sorting-room: implement step 2 and return an error for negative fodder

--- the-farm/the_farm.go
+++ the-farm/the_farm.go
@@ -1,17 +1,27 @@
 package thefarm
 
+import "errors"
+
 // See types.go for the types defined for this exercise.
 
 // TODO: Define the SillyNephewError type here.
 
 // DivideFood computes the fodder amount per cow for the given cows.
 func DivideFood(weightFodder WeightFodder, cows int) (float64, error) {
+
 	amount, err := weightFodder.FodderAmount()
 	if err != nil {
 		if err == ErrScaleMalfunction {
-			return amount * 1 / float64(cows), nil
+			if amount < 0 {
+				return 0.0, errors.New("negative fodder")
+			}
+			return amount * 2 / float64(cows), nil
 		}
 		return 0, err
+	}
+
+	if amount < 0 {
+		return 0.0, errors.New("negative fodder")
 	}
 
 	return amount / float64(cows), nil
