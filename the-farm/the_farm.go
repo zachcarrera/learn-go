88caa2127874575cf56686459fa1db1abcd6d84d
Zach Carrera <zachcarrera@gmail.com>
2023-06-12 17:33:33 -0700

    the-farm: implement step 1 and get the amount of fodder from FodderAmount

--- the-farm/the_farm.go
+++ the-farm/the_farm.go
@@ -6,5 +6,13 @@ package thefarm
 
 // DivideFood computes the fodder amount per cow for the given cows.
 func DivideFood(weightFodder WeightFodder, cows int) (float64, error) {
-	panic("Please implement DivideFood")
+	amount, err := weightFodder.FodderAmount()
+	if err != nil {
+		if err == ErrScaleMalfunction {
+			return amount * 1 / float64(cows), nil
+		}
+		return 0, err
+	}
+
+	return amount / float64(cows), nil
 }
