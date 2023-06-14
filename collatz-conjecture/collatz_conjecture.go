5da54306059144641454255e527b0dcfaaf3a6c4
Zach Carrera <zachcarrera@gmail.com>
2023-06-13 18:44:09 -0700

    collatz-conjecture: implement CollatzConjecture

--- collatz-conjecture/collatz_conjecture.go
+++ collatz-conjecture/collatz_conjecture.go
@@ -1,5 +1,19 @@
 package collatzconjecture
 
+import "errors"
+
 func CollatzConjecture(n int) (int, error) {
-	panic("Please implement the CollatzConjecture function")
+	if n <= 0 {
+		return 0, errors.New("input must be a positive number")
+	}
+	count := 0
+	for n != 1 {
+		if n%2 == 0 {
+			n /= 2
+		} else {
+			n = 3*n + 1
+		}
+		count += 1
+	}
+	return count, nil
 }
