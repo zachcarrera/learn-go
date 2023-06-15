272cb40abcfa87a38d7dd57a0992210006c770ba
Zach Carrera <zachcarrera@gmail.com>
2023-06-14 21:47:18 -0700

    scrabble-score: implement Score

--- scrabble-score/scrabble_score.go
+++ scrabble-score/scrabble_score.go
@@ -1,5 +1,30 @@
 package scrabble
 
+import "strings"
+
 func Score(word string) int {
-	panic("Please implement the Score function")
+	word = strings.ToLower(word)
+	score := 0
+	for _, char := range word {
+
+		switch char {
+		case 'a', 'e', 'i', 'o', 'u', 'l', 'n', 'r', 's', 't':
+			score += 1
+		case 'd', 'g':
+			score += 2
+		case 'b', 'c', 'm', 'p':
+			score += 3
+		case 'f', 'h', 'v', 'w', 'y':
+			score += 4
+		case 'k':
+			score += 5
+		case 'j', 'x':
+			score += 8
+		case 'q', 'z':
+			score += 10
+		default:
+			score += 0
+		}
+	}
+	return score
 }
