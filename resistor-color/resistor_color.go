27519d7fbd8170365854a3e3983e86a0a1392dfe
Zach Carrera <zachcarrera@gmail.com>
2023-06-15 21:13:21 -0700

    resistor-color: implement ColorCode and refactor Colors strings

--- resistor-color/resistor_color.go
+++ resistor-color/resistor_color.go
@@ -3,20 +3,26 @@ package resistorcolor
 // Colors should return the list of all colors.
 func Colors() []string {
 	return []string{
-		"Black:",
-		"Brown:",
-		"Red:",
-		"Orange:",
-		"Yellow:",
-		"Green:",
-		"Blue:",
-		"Violet:",
-		"Grey:",
-		"White:",
+		"black",
+		"brown",
+		"red",
+		"orange",
+		"yellow",
+		"green",
+		"blue",
+		"violet",
+		"grey",
+		"white",
 	}
 }
 
 // ColorCode returns the resistance value of the given color.
 func ColorCode(color string) int {
-	panic("Please implement the ColorCode function")
+	colors := Colors()
+	for i, currentColor := range colors {
+		if currentColor == color {
+			return i
+		}
+	}
+	return -1
 }
