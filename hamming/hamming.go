3f4934cbd6ed57139300aa6450f1b4759cad31c6
Zach Carrera <zachcarrera@gmail.com>
2023-06-16 12:49:36 -0700

    hamming: implement Distance

--- hamming/hamming.go
+++ hamming/hamming.go
@@ -1,5 +1,17 @@
 package hamming
 
+import "errors"
+
 func Distance(a, b string) (int, error) {
-	panic("Implement the Distance function")
+	if len(a) != len(b) {
+		return 0, errors.New("sequences must match")
+	}
+	differences := 0
+	bSlice := []rune(b)
+	for i, char := range a {
+		if char != bSlice[i] {
+			differences += 1
+		}
+	}
+	return differences, nil
 }
