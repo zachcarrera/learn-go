de19c695e427b546cb30170292af2bc34f878b95
Zach Carrera <zachcarrera@gmail.com>
2023-06-16 21:11:54 -0700

    luhn: remove whitespace and convert string of digits to slice of ints

--- luhn/luhn.go
+++ luhn/luhn.go
@@ -1,9 +1,24 @@
 package luhn
 
+import (
+	"fmt"
+	"strings"
+	"unicode"
+)
+
 func Valid(id string) bool {
 	if len(id) <= 1 {
 		return false
 	}
+	id = strings.Join(strings.Fields(id), "")
+	var digits []int
+	for _, char := range id {
+		if !unicode.IsDigit(char) {
+			return false
+		}
+		digits = append(digits, int(char-'0'))
+	}
+	fmt.Println(digits)
 
 	return true
 }
