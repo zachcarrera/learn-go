e5128125dd76317d73bc3e8a2de68fa557ec5fbc
Zach Carrera <zachcarrera@gmail.com>
2023-06-16 23:50:43 -0700

    luhn: finish Valid implementation to double every second digit from the back and check if the sum of all digits is divisible by 10 NOTE: one test still failing

--- luhn/luhn.go
+++ luhn/luhn.go
@@ -1,7 +1,6 @@
 package luhn
 
 import (
-	"fmt"
 	"strings"
 	"unicode"
 )
@@ -18,7 +17,20 @@ func Valid(id string) bool {
 		}
 		digits = append(digits, int(char-'0'))
 	}
-	fmt.Println(digits)
 
-	return true
+	sum := 0
+	for i := range digits {
+		k := len(digits) - 1 - i
+
+		if i%2 == 1 {
+			digits[k] *= 2
+			if digits[k] > 9 {
+				digits[k] -= 9
+			}
+		}
+		sum += digits[k]
+	}
+
+	// FIXME: edge case where the number is only zeros
+	return sum%10 == 0 && sum > 0
 }
