295030197a1eddef7a6cc23cbbb742b54b6ea757
Zach Carrera <zachcarrera@gmail.com>
2023-06-19 16:07:19 -0700

    isogram: implement IsIsogram

--- isogram/isogram.go
+++ isogram/isogram.go
@@ -1,5 +1,21 @@
 package isogram
 
+import "strings"
+
 func IsIsogram(word string) bool {
-	panic("Please implement the IsIsogram function")
+	word = strings.ToLower(word)
+
+	// build a frequency table for word and return false if
+	// there is a duplicate char that is not a space or hyphen
+	frequencyTable := map[rune]int{}
+	for _, char := range word {
+		if char == ' ' || char == '-' {
+			continue
+		}
+		if frequencyTable[char] >= 1 {
+			return false
+		}
+		frequencyTable[char] += 1
+	}
+	return true
 }
