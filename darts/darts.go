90e83e5b9b09233489147b8f93b1a69f13a7bc6f
Zach Carrera <zachcarrera@gmail.com>
2023-06-20 13:55:48 -0700

    darts: implement Score

--- darts/darts.go
+++ darts/darts.go
@@ -1,5 +1,22 @@
 package darts
 
+import "math"
+
 func Score(x, y float64) int {
-	panic("Please implement the Score function")
+	// find direct distance from center (0, 0)
+	// use that distance to calculate points
+
+	// a^2 + b^2 = c^2
+	// c = sqrt(a^2 + b^2)
+	distance := math.Sqrt(math.Pow(x, 2) + math.Pow(y, 2))
+	switch {
+	case distance <= 1:
+		return 10
+	case distance <= 5:
+		return 5
+	case distance <= 10:
+		return 1
+	default:
+		return 0
+	}
 }
