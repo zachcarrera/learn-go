c826fc140297c21346b59631194abcc7823aa982
Zach Carrera <zachcarrera@gmail.com>
2023-06-19 18:36:18 -0700

    rna-transcription: implement ToRna

--- rna-transcription/rna_transcription.go
+++ rna-transcription/rna_transcription.go
@@ -1,5 +1,17 @@
 package strand
 
 func ToRNA(dna string) string {
-	panic("Please implement the ToRNA function")
+	dnaToRnaMap := map[rune]rune{
+		'C': 'G',
+		'G': 'C',
+		'T': 'A',
+		'A': 'U',
+	}
+	dnaCharacters := []rune(dna)
+
+	for i, char := range dnaCharacters {
+		dnaCharacters[i] = dnaToRnaMap[char]
+	}
+
+	return string(dnaCharacters)
 }
