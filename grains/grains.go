ecc991c78a688b3858cca7cfc01f2b20f5b9a06f
Zach Carrera <zachcarrera@gmail.com>
2023-06-20 15:00:10 -0700

    grains: implement Square

--- grains/grains.go
+++ grains/grains.go
@@ -1,7 +1,15 @@
 package grains
 
+import (
+	"errors"
+	"math"
+)
+
 func Square(number int) (uint64, error) {
-	panic("Please implement the Square function")
+	if number <= 0 || number > 64 {
+		return 0.0, errors.New("invalid number")
+	}
+	return uint64(math.Pow(2, float64(number-1))), nil
 }
 
 func Total() uint64 {
