37257376bf86692e042475eb9dcf2dde0c1e1a27
Zach Carrera <zachcarrera@gmail.com>
2023-06-19 17:05:59 -0700

    nucleotide-count: implement Counts

--- nucleotide-count/nucleotide_count.go
+++ nucleotide-count/nucleotide_count.go
@@ -1,19 +1,34 @@
 package dna
 
+import "errors"
+
 // Histogram is a mapping from nucleotide to its count in given DNA.
 // Choose a suitable data type.
-type Histogram
+type Histogram map[rune]int
 
 // DNA is a list of nucleotides. Choose a suitable data type.
-type DNA
+type DNA string
 
 // Counts generates a histogram of valid nucleotides in the given DNA.
 // Returns an error if d contains an invalid nucleotide.
-///
+
 // Counts is a method on the DNA type. A method is a function with a special receiver argument.
 // The receiver appears in its own argument list between the func keyword and the method name.
 // Here, the Counts method has a receiver of type DNA named d.
 func (d DNA) Counts() (Histogram, error) {
-	var h Histogram
+
+	h := map[rune]int{}
+	h['A'] = 0
+	h['C'] = 0
+	h['G'] = 0
+	h['T'] = 0
+
+	for _, char := range d {
+		// A C G T check
+		if !(char == 'A' || char == 'C' || char == 'G' || char == 'T') {
+			return nil, errors.New("Invalid String")
+		}
+		h[char] += 1
+	}
 	return h, nil
 }
