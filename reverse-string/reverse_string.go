77272c8256e443727cd894e6bd184e52ceaecb2c
Zach Carrera <zachcarrera@gmail.com>
2023-06-21 19:34:20 -0700

    reverse-string: add comments for Reverse

--- reverse-string/reverse_string.go
+++ reverse-string/reverse_string.go
@@ -1,6 +1,8 @@
 package reverse
 
 func Reverse(input string) string {
+	// convert the input string into a slice of runes
+	// so multibyte chars can still be reversed
 	inputRunes := []rune(input)
 	var reversed []rune
 	for i := len(inputRunes) - 1; i >= 0; i-- {
