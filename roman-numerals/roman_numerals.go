efe688039073b23311300b59f9c354c39f576357
Zach Carrera <zachcarrera@gmail.com>
2023-06-21 23:45:51 -0700

    roman-numerals: refactor ToRomanNumeral to use a slice of custom structs and range over it to build the converted string

--- roman-numerals/roman_numerals.go
+++ roman-numerals/roman_numerals.go
@@ -2,83 +2,38 @@ package romannumerals
 
 import "errors"
 
+type RomanConversion struct {
+	arabic int
+	roman  string
+}
+
 func ToRomanNumeral(input int) (string, error) {
 	if input > 3999 || input < 1 {
 		return "", errors.New("input must be between 1 and 3999")
 	}
 
+	dictionary := []RomanConversion{
+		{1000, "M"},
+		{900, "CM"},
+		{500, "D"},
+		{400, "CD"},
+		{100, "C"},
+		{90, "XC"},
+		{50, "L"},
+		{40, "XL"},
+		{10, "X"},
+		{9, "IX"},
+		{5, "V"},
+		{4, "IV"},
+		{1, "I"},
+	}
+
 	var output string
-	switch input % 10 {
-	case 1:
-		output = "I"
-	case 2:
-		output = "II"
-	case 3:
-		output = "III"
-	case 4:
-		output = "IV"
-	case 5:
-		output = "V"
-	case 6:
-		output = "VI"
-	case 7:
-		output = "VII"
-	case 8:
-		output = "VIII"
-	case 9:
-		output = "IX"
+	for _, conversion := range dictionary {
+		for input >= conversion.arabic {
+			output += conversion.roman
+			input -= conversion.arabic
+		}
 	}
-
-	switch (input / 10) % 10 {
-	case 1:
-		output = "X" + output
-	case 2:
-		output = "XX" + output
-	case 3:
-		output = "XXX" + output
-	case 4:
-		output = "XL" + output
-	case 5:
-		output = "L" + output
-	case 6:
-		output = "LX" + output
-	case 7:
-		output = "LXX" + output
-	case 8:
-		output = "LXXX" + output
-	case 9:
-		output = "XC" + output
-	}
-
-	switch (input / 100) % 10 {
-	case 1:
-		output = "C" + output
-	case 2:
-		output = "CC" + output
-	case 3:
-		output = "CCC" + output
-	case 4:
-		output = "CD" + output
-	case 5:
-		output = "D" + output
-	case 6:
-		output = "DC" + output
-	case 7:
-		output = "DCC" + output
-	case 8:
-		output = "DCCC" + output
-	case 9:
-		output = "CM" + output
-	}
-
-	switch (input / 1000) % 10 {
-	case 1:
-		output = "M" + output
-	case 2:
-		output = "MM" + output
-	case 3:
-		output = "MMM" + output
-	}
-
 	return output, nil
 }
