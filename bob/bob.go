2a6d71b9201062d5bb1dbf4fd8fa4791c6355ae9
Zach Carrera <zachcarrera@gmail.com>
2023-06-22 15:05:23 -0700

    bob: add allCapsRegEx for Hey

--- bob/bob.go
+++ bob/bob.go
@@ -5,11 +5,24 @@
 // https://golang.org/doc/effective_go.html#commentary
 package bob
 
+import (
+	"fmt"
+	"regexp"
+)
+
 // Hey should have a comment documenting it.
 func Hey(remark string) string {
 	// Write some code here to pass the test suite.
 	// Then remove all the stock comments.
 	// They're here to help you get started but they only clutter a finished solution.
 	// If you leave them in, reviewers may protest!
-	return ""
+	fmt.Println("===== =====")
+	fmt.Println(remark)
+	// will match when all charachters are not lowercase
+	allCapsRegEx := regexp.MustCompile(`^[^a-z]+$`)
+	if allCapsRegEx.MatchString(remark) {
+		fmt.Printf("%s: %t\n", "all caps regex", allCapsRegEx.MatchString(remark))
+		return "Whoa, chill out!"
+	}
+	return "Whatever."
 }
