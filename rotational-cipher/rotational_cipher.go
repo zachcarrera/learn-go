1799f2c5aec499a68799dc0ac8d54dbe13410fea
Zach Carrera <zachcarrera@gmail.com>
2023-06-23 21:21:09 -0700

    rotational-cipher: implement RotationalCipher

--- rotational-cipher/rotational_cipher.go
+++ rotational-cipher/rotational_cipher.go
@@ -1,5 +1,26 @@
 package rotationalcipher
 
+import "unicode"
+
 func RotationalCipher(plain string, shiftKey int) string {
-	panic("Please implement the RotationalCipher function")
+	shiftKey = shiftKey % 26
+	rotated := ""
+	for _, char := range plain {
+		if unicode.IsUpper(char) {
+			new := char + rune(shiftKey)
+			if new > 90 {
+				new -= 26
+			}
+			rotated += string(new)
+		} else if unicode.IsLower(char) {
+			new := char + rune(shiftKey)
+			if new > 122 {
+				new -= 26
+			}
+			rotated += string(new)
+		} else {
+			rotated += string(char)
+		}
+	}
+	return rotated
 }
