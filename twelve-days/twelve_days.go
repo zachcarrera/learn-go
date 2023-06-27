e2c43c38b9944b68da6841fa83041770957f26af
Zach Carrera <zachcarrera@gmail.com>
2023-06-26 17:35:21 -0700

    twelve-days: create the global gifts slice

--- twelve-days/twelve_days.go
+++ twelve-days/twelve_days.go
@@ -1,5 +1,20 @@
 package twelve
 
+var gifts = []string{
+	"a Partridge in a Pear Tree",
+	"two Turtle Doves",
+	"three French Hens",
+	"four Calling Birds",
+	"five Gold Rings",
+	"six Geese-a-Laying",
+	"seven Swans-a-Swimming",
+	"eight Maids-a-Milking",
+	"nine Ladies Dancing",
+	"ten Lords-a-Leaping",
+	"eleven Pipers Piping",
+	"twelve Drummers Drumming",
+}
+
 func Verse(i int) string {
 	panic("Please implement the Verse function")
 }
