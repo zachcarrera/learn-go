30b3ab44d4d943bbf1ba1c5651c2d1f91d18debf
Zach Carrera <zachcarrera@gmail.com>
2023-06-27 14:08:37 -0700

    twelve-days: refactor global vars

--- twelve-days/twelve_days.go
+++ twelve-days/twelve_days.go
@@ -5,35 +5,37 @@ import (
 	"strings"
 )
 
-var gifts = []string{
-	"a Partridge in a Pear Tree",
-	"two Turtle Doves",
-	"three French Hens",
-	"four Calling Birds",
-	"five Gold Rings",
-	"six Geese-a-Laying",
-	"seven Swans-a-Swimming",
-	"eight Maids-a-Milking",
-	"nine Ladies Dancing",
-	"ten Lords-a-Leaping",
-	"eleven Pipers Piping",
-	"twelve Drummers Drumming",
-}
+var (
+	gifts = []string{
+		"a Partridge in a Pear Tree",
+		"two Turtle Doves",
+		"three French Hens",
+		"four Calling Birds",
+		"five Gold Rings",
+		"six Geese-a-Laying",
+		"seven Swans-a-Swimming",
+		"eight Maids-a-Milking",
+		"nine Ladies Dancing",
+		"ten Lords-a-Leaping",
+		"eleven Pipers Piping",
+		"twelve Drummers Drumming",
+	}
 
-var days = []string{
-	"first",
-	"second",
-	"third",
-	"fourth",
-	"fifth",
-	"sixth",
-	"seventh",
-	"eighth",
-	"ninth",
-	"tenth",
-	"eleventh",
-	"twelfth",
-}
+	days = []string{
+		"first",
+		"second",
+		"third",
+		"fourth",
+		"fifth",
+		"sixth",
+		"seventh",
+		"eighth",
+		"ninth",
+		"tenth",
+		"eleventh",
+		"twelfth",
+	}
+)
 
 func Verse(i int) string {
 	prefix := fmt.Sprintf("On the %s day of Christmas my true love gave to me:", days[i-1])
