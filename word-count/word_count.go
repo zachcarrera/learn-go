4c0d387bce65ad02e9572f36cf5e3eed7276db0c
Zach Carrera <zachcarrera@gmail.com>
2023-06-30 23:29:41 -0700

    word-count: finish implementation of WordCount by switching regex matching and finding all strings instead of splitting on a match

--- word-count/word_count.go
+++ word-count/word_count.go
@@ -9,18 +9,13 @@ type Frequency map[string]int
 
 func WordCount(phrase string) Frequency {
 	phrase = strings.ToLower(phrase)
-	phrase = strings.TrimSpace(phrase)
-	// words := strings.Split(phrase, " ")
-	myRegex := regexp.MustCompile(`[\s,:!&@\$%\^"\.]`)
-	words := myRegex.Split(phrase, -1)
+	re := regexp.MustCompile(`\w+('\w+)?`)
+	words := re.FindAllString(phrase, -1)
 
-	// var frequency map[string]int
 	frequency := map[string]int{}
 
 	for _, word := range words {
-		if word != "" && word != "'" {
-			frequency[word] += 1
-		}
+		frequency[word] += 1
 	}
 
 	return frequency
