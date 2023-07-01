8894a37a121235452263ca1c14a2abebde96a2ef
Zach Carrera <zachcarrera@gmail.com>
2023-06-30 23:19:44 -0700

    word-count: implement regex for all punctuation except apostrophes

--- word-count/word_count.go
+++ word-count/word_count.go
@@ -11,14 +11,14 @@ func WordCount(phrase string) Frequency {
 	phrase = strings.ToLower(phrase)
 	phrase = strings.TrimSpace(phrase)
 	// words := strings.Split(phrase, " ")
-	myRegex := regexp.MustCompile(`[\s,]`)
+	myRegex := regexp.MustCompile(`[\s,:!&@\$%\^"\.]`)
 	words := myRegex.Split(phrase, -1)
 
 	// var frequency map[string]int
 	frequency := map[string]int{}
 
 	for _, word := range words {
-		if word != "" {
+		if word != "" && word != "'" {
 			frequency[word] += 1
 		}
 	}
