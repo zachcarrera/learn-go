1d5c5e9c9d434bcd41f55db25f544eed841b8cba
Zach Carrera <zachcarrera@gmail.com>
2023-06-30 22:52:19 -0700

    word-count: split the phrase in any whitespace or comma and create a Frequency

--- word-count/word_count.go
+++ word-count/word_count.go
@@ -1,7 +1,27 @@
 package wordcount
 
+import (
+	"regexp"
+	"strings"
+)
+
 type Frequency map[string]int
 
 func WordCount(phrase string) Frequency {
-	panic("Please implement the WordCount function")
+	phrase = strings.ToLower(phrase)
+	phrase = strings.TrimSpace(phrase)
+	// words := strings.Split(phrase, " ")
+	myRegex := regexp.MustCompile(`[\s,]`)
+	words := myRegex.Split(phrase, -1)
+
+	// var frequency map[string]int
+	frequency := map[string]int{}
+
+	for _, word := range words {
+		if word != "" {
+			frequency[word] += 1
+		}
+	}
+
+	return frequency
 }
