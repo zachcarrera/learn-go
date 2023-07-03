1c7aae37f93f88e8118af32658caebe4a34dcc28
Zach Carrera <zachcarrera@gmail.com>
2023-07-03 12:31:47 -0700

    isbn-verifier: remove "-" from ibsn

--- isbn-verifier/isbn_verifier.go
+++ isbn-verifier/isbn_verifier.go
@@ -1,8 +1,18 @@
 package isbn
 
+import (
+	"fmt"
+	"strings"
+)
+
 func IsValidISBN(isbn string) bool {
-	if len(isbn) == 10 || len(isbn) == 13 {
-		return true
+	if !(len(isbn) == 10 || len(isbn) == 13) {
+		return false
 	}
-	return false
+	fmt.Printf("isbn: %v\n", isbn)
+	groups := strings.Split(isbn, "-")
+	fmt.Printf("groups: %v\n", groups)
+	isbn = strings.Join(groups, "")
+	fmt.Printf("isbn: %v\n", isbn)
+	return true
 }
