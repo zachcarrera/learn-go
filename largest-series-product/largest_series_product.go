ac183b73784226db2e7a61459cd1680e59cb7d96
Zach Carrera <zachcarrera@gmail.com>
2023-07-06 23:14:35 -0700

    largest-series-product: add error handling for LargestSeriesProduct

--- largest-series-product/largest_series_product.go
+++ largest-series-product/largest_series_product.go
@@ -1,5 +1,23 @@
 package lsproduct
 
+import (
+	"errors"
+	"unicode"
+)
+
 func LargestSeriesProduct(digits string, span int) (int64, error) {
-	panic("Please implement the LargestSeriesProduct function")
+	if len(digits) < span {
+		return 0, errors.New("Span cannot be larger than the input length.")
+	}
+	if span < 0 {
+		return 0, errors.New("Span must not be negative")
+	}
+
+	for _, char := range digits {
+		if !unicode.IsDigit(char) {
+			return 0, errors.New("Digits input must contain only digits")
+		}
+	}
+
+	return 0, nil
 }
