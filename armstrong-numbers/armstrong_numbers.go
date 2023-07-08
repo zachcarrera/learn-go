40b9297eb618eabd40c822b91ff6b4eeb1d92334
Zach Carrera <zachcarrera@gmail.com>
2023-07-07 22:59:32 -0700

    armstrong-numbers: implement IsNumber

--- armstrong-numbers/armstrong_numbers.go
+++ armstrong-numbers/armstrong_numbers.go
@@ -1,5 +1,15 @@
 package armstrong
 
+import (
+	"math"
+	"strconv"
+)
+
 func IsNumber(n int) bool {
-	panic("Please implement the IsNumber function")
+	numberAsString := strconv.Itoa(n)
+	sum := 0
+	for _, num := range numberAsString {
+		sum += int(math.Pow(float64(num-'0'), float64(len(numberAsString))))
+	}
+	return sum == n
 }
