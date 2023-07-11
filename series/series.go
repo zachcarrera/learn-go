30c1a44431ec60b6e1575109183450f2808dd30f
Zach Carrera <zachcarrera@gmail.com>
2023-07-11 11:25:12 -0700

    series: implement All

--- series/series.go
+++ series/series.go
@@ -1,7 +1,19 @@
 package series
 
 func All(n int, s string) []string {
-	panic("Please implement the All function")
+	if n > len(s) {
+		return []string{}
+	}
+	subStrings := make([]string, len(s)-n+1)
+	characters := []rune(s)
+	for i := 0; i < len(characters)-n+1; i++ {
+		subString := make([]rune, n)
+		for j := 0; j < n; j++ {
+			subString[j] = characters[i+j]
+		}
+		subStrings[i] = string(subString)
+	}
+	return subStrings
 }
 
 func UnsafeFirst(n int, s string) string {
