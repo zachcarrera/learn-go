18d9a6e33a3cdbb026327a2c81eda23038f0fe91
Zach Carrera <zachcarrera@gmail.com>
2023-07-12 18:40:08 -0700

    nth-prime: finish Nth implementation

--- nth-prime/nth_prime.go
+++ nth-prime/nth_prime.go
@@ -7,5 +7,21 @@ func Nth(n int) (int, error) {
 	if n <= 0 {
 		return 0, errors.New("Nth prime number cannot be calculated")
 	}
-	panic("Please implement the Nth function")
+	primes := []int{2}
+	nextNumber := 3
+	for len(primes) < n {
+		isPrime := true
+		for _, prime := range primes {
+			if nextNumber%prime == 0 {
+				isPrime = false
+				nextNumber += 2
+				break
+			}
+		}
+		if isPrime {
+			primes = append(primes, nextNumber)
+			nextNumber += 2
+		}
+	}
+	return primes[n-1], nil
 }
