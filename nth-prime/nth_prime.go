9fe9fdb3f643a7a020eecc1f6c99bcbbe37c0f43
Zach Carrera <zachcarrera@gmail.com>
2023-07-12 17:58:47 -0700

    nth-prime: implement error handling for Nth

--- nth-prime/nth_prime.go
+++ nth-prime/nth_prime.go
@@ -1,6 +1,11 @@
 package prime
 
+import "errors"
+
 // Nth returns the nth prime number. An error must be returned if the nth prime number can't be calculated ('n' is equal or less than zero)
 func Nth(n int) (int, error) {
+	if n <= 0 {
+		return 0, errors.New("Nth prime number cannot be calculated")
+	}
 	panic("Please implement the Nth function")
 }
