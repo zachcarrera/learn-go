06598df0db1ed1590f582fd1b4362cd4790f2ec2
Zach Carrera <zachcarrera@gmail.com>
2023-07-18 16:19:24 -0700

    binary-search: implement SearchInts

--- binary-search/binary_search.go
+++ binary-search/binary_search.go
@@ -1,5 +1,18 @@
 package binarysearch
 
 func SearchInts(list []int, key int) int {
-	panic("Please implement the SearchInts function")
+	low, high := 0, len(list)-1
+
+	for high >= low {
+		middle := (high + low) / 2
+		if list[middle] == key {
+			return middle
+		} else if list[middle] < key {
+			low = middle + 1
+		} else {
+			high = middle - 1
+		}
+	}
+
+	return -1
 }
