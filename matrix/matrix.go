d35a6f80cc45d5de34e0435ed9251a508887a639
Zach Carrera <zachcarrera@gmail.com>
2023-07-17 19:09:31 -0700

    matrix: implement New

--- matrix/matrix.go
+++ matrix/matrix.go
@@ -1,10 +1,39 @@
 package matrix
 
+import (
+	"errors"
+	"strconv"
+	"strings"
+)
+
 // Define the Matrix type here.
 type Matrix [][]int
 
 func New(s string) (Matrix, error) {
-	panic("Please implement the New function")
+
+	rows := strings.Split(s, "\n")
+	m := make(Matrix, 0, len(rows))
+	var rowLength int
+	for i, row := range rows {
+		num := strings.Split(strings.TrimSpace(row), " ")
+		var numbers []int
+		for _, v := range num {
+			number, err := strconv.Atoi(v)
+			if err != nil {
+				return nil, errors.New("test error")
+			}
+			numbers = append(numbers, number)
+		}
+		if i == 0 {
+			rowLength = len(numbers)
+		} else {
+			if len(numbers) != rowLength {
+				return nil, errors.New("length error")
+			}
+		}
+		m = append(m, numbers)
+	}
+	return m, nil
 }
 
 // Cols and Rows must return the results without affecting the matrix.
