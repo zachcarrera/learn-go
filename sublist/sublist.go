fcdd1d6796163fa5c12eb68800b69976c92fc8be
Zach Carrera <zachcarrera@gmail.com>
2023-07-18 15:15:21 -0700

    sublist: account for base case and for when two lists of equal length

--- sublist/sublist.go
+++ sublist/sublist.go
@@ -1,7 +1,16 @@
 package sublist
 
+import "reflect"
+
 // Relation type is defined in relations.go file.
 
 func Sublist(l1, l2 []int) Relation {
+	if len(l1) == len(l2) {
+		if reflect.DeepEqual(l1, l2) {
+			return RelationEqual
+		} else {
+			return RelationUnequal
+		}
+	return RelationUnequal
 	panic("Please implement the Sublist function")
 }
