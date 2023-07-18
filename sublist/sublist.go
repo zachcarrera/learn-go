c583c1e944ed39d873ffcb1ec2efd22d0b7805bf
Zach Carrera <zachcarrera@gmail.com>
2023-07-18 15:35:51 -0700

    sublist: refactor Sublist with a switch statement

--- sublist/sublist.go
+++ sublist/sublist.go
@@ -5,20 +5,19 @@ import "reflect"
 // Relation type is defined in relations.go file.
 
 func Sublist(l1, l2 []int) Relation {
-	if len(l1) == len(l2) {
-		if reflect.DeepEqual(l1, l2) {
-			return RelationEqual
-		} else {
-			return RelationUnequal
+	length1, length2 := len(l1), len(l2)
+
+	switch {
+	case length1 == length2 && reflect.DeepEqual(l1, l2):
+		return RelationEqual
+	case length1 > length2:
+		opposite := Sublist(l2, l1)
+		if opposite == RelationSublist {
+			return RelationSuperlist
 		}
-	} else if len(l1) > len(l2) {
-		if result := Sublist(l2, l1); result != "sublist" {
-			return result
-		}
-		return RelationSuperlist
-	} else {
-		for i := 0; i <= len(l2)-len(l1); i++ {
-			if reflect.DeepEqual(l1, l2[i:i+len(l1)]) {
+	default:
+		for i := 0; i <= length2-length1; i++ {
+			if reflect.DeepEqual(l1, l2[i:i+length1]) {
 				return RelationSublist
 			}
 		}
