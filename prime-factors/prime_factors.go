f92f7bd6dd3f86b1f59d42ad198a2c76be99cdff
Zach Carrera <zachcarrera@gmail.com>
2023-07-19 22:38:33 -0700

    prime-factors: implement Factors

--- prime-factors/prime_factors.go
+++ prime-factors/prime_factors.go
@@ -1,5 +1,15 @@
 package prime
 
 func Factors(n int64) []int64 {
-	panic("Please implement the Factors function")
+	var primeFactors []int64
+	factor := int64(2)
+	for factor <= n {
+		if n%factor == 0 {
+			n /= factor
+			primeFactors = append(primeFactors, factor)
+		} else {
+			factor++
+		}
+	}
+	return primeFactors
 }
