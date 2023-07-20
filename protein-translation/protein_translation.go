48bf55915c76d6671f209941aba58922a197a524
Zach Carrera <zachcarrera@gmail.com>
2023-07-19 21:59:01 -0700

    protein-translation: create ErrStop and ErrInvalidBase

--- protein-translation/protein_translation.go
+++ protein-translation/protein_translation.go
@@ -1,5 +1,10 @@
 package protein
 
+import "errors"
+
+var ErrStop = errors.New("Stop code was matched")
+var ErrInvalidBase = errors.New("Invalid Codon detected")
+
 func FromRNA(rna string) ([]string, error) {
 	panic("Please implement the FromRNA function")
 }
