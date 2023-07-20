7a639db8891552b23872a2d50a372a628b8e30f7
Zach Carrera <zachcarrera@gmail.com>
2023-07-19 18:17:47 -0700

    matching-brackets: implement Bracket

--- matching-brackets/matching_brackets.go
+++ matching-brackets/matching_brackets.go
@@ -1,5 +1,17 @@
 package brackets
 
 func Bracket(input string) bool {
-	panic("Please implement the Bracket function")
+	stack := make([]rune, 0)
+	for _, char := range input {
+		if char == '{' || char == '[' || char == '(' {
+			stack = append(stack, char)
+		}
+
+		if len(stack) > 0 && ((char == '}' && stack[len(stack)-1] == '{') || (char == ']' && stack[len(stack)-1] == '[') || (char == ')' && stack[len(stack)-1] == '(')) {
+			stack = stack[:len(stack)-1]
+		} else if char == '}' || char == ']' || char == ')' {
+			return false
+		}
+	}
+	return len(stack) == 0
 }
