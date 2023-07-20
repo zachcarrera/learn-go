3c5d01423e0970d59b305dd26036017f7ccbf72d
Zach Carrera <zachcarrera@gmail.com>
2023-07-19 22:02:09 -0700

    protein-translation: implement FromCodon

--- protein-translation/protein_translation.go
+++ protein-translation/protein_translation.go
@@ -10,5 +10,26 @@ func FromRNA(rna string) ([]string, error) {
 }
 
 func FromCodon(codon string) (string, error) {
-	panic("Please implement the FromCodon function")
+	var protein string
+	switch codon {
+	case "AUG":
+		protein = "Methionine"
+	case "UUU", "UUC":
+		protein = "Phenylalanine"
+	case "UUA", "UUG":
+		protein = "Leucine"
+	case "UCU", "UCC", "UCA", "UCG":
+		protein = "Serine"
+	case "UAU", "UAC":
+		protein = "Tyrosine"
+	case "UGU", "UGC":
+		protein = "Cysteine"
+	case "UGG":
+		protein = "Tryptophan"
+	case "UAA", "UAG", "UGA":
+		return "", ErrStop
+	default:
+		return "", ErrInvalidBase
+	}
+	return protein, nil
 }
