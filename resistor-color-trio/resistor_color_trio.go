14be180e916a51c262f4274f2a273969088251ea
Zach Carrera <zachcarrera@gmail.com>
2023-07-20 20:34:35 -0700

    resistor-color-trio: implement Label

--- resistor-color-trio/resistor_color_trio.go
+++ resistor-color-trio/resistor_color_trio.go
@@ -1,5 +1,11 @@
 package resistorcolortrio
 
+import (
+	"math"
+	"strconv"
+	"strings"
+)
+
 var (
 	resistors = map[string]int{
 		"black":  0,
@@ -26,5 +32,22 @@ var (
 // The label is a string with a resistance value with an unit appended
 // (e.g. "33 ohms", "470 kiloohms").
 func Label(colors []string) string {
-	panic("Implement the Label function")
+	if len(colors) < 3 {
+		return ""
+	}
+	var sb strings.Builder
+	base := resistors[colors[0]]*10 + resistors[colors[1]]
+	numberOfZeros := resistors[colors[2]]
+	multiplier := 1 * int(math.Pow10(numberOfZeros))
+	base *= multiplier
+	count := 0
+
+	for len(strconv.Itoa(base)) > 3 {
+		base /= 1000
+		count++
+	}
+	sb.WriteString(strconv.Itoa(base))
+	sb.WriteRune(' ')
+	sb.WriteString(units[count])
+	return sb.String()
 }
