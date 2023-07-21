5577b5e020da1908c328114d9c940531fd5ed6ad
Zach Carrera <zachcarrera@gmail.com>
2023-07-21 13:38:56 -0700

    secret-handshake: implement Handshake to create the correct slice based off code

--- secret-handshake/secret_handshake.go
+++ secret-handshake/secret_handshake.go
@@ -3,5 +3,17 @@ package secret
 var actions = []string{"wink", "double blink", "close your eyes", "jump"}
 
 func Handshake(code uint) []string {
-	panic("Please implement the Handshake function")
+	var handshake []string
+	for i, action := range actions {
+		if code&(1<<i) != 0 {
+			handshake = append(handshake, action)
+		}
+	}
+	if code&(1<<4) != 0 {
+		for i, j := 0, len(handshake)-1; i < j; i, j = i+1, j-1 {
+			handshake[i], handshake[j] = handshake[j], handshake[i]
+		}
+
+	}
+	return handshake
 }
