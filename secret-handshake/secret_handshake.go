51fe404b98d2202251f2d0908adaa7c2e6019c01
Zach Carrera <zachcarrera@gmail.com>
2023-07-21 13:38:14 -0700

    secret-handshake: create actions slice

--- secret-handshake/secret_handshake.go
+++ secret-handshake/secret_handshake.go
@@ -1,5 +1,7 @@
 package secret
 
+var actions = []string{"wink", "double blink", "close your eyes", "jump"}
+
 func Handshake(code uint) []string {
 	panic("Please implement the Handshake function")
 }
