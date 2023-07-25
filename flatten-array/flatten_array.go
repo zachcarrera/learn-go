14aa5115aade0681b8249194a8c1344d6f7aa0a1
Zach Carrera <zachcarrera@gmail.com>
2023-07-25 11:29:41 -0700

    flatten-array: implement Flatten

--- flatten-array/flatten_array.go
+++ flatten-array/flatten_array.go
@@ -1,5 +1,18 @@
 package flatten
 
+import "reflect"
+
 func Flatten(nested interface{}) []interface{} {
-	panic("Please implement the Flatten function")
+	flattened := make([]interface{}, 0)
+	nestedTyped := nested.([]interface{})
+	for _, v := range nestedTyped {
+		if v == nil {
+			continue
+		} else if reflect.TypeOf(v).Kind() == reflect.Slice {
+			flattened = append(flattened, Flatten(v)...)
+		} else {
+			flattened = append(flattened, v)
+		}
+	}
+	return flattened
 }
