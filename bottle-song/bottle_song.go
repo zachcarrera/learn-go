942af535c825b2cac8d72a389d617aae2b00251b
Zach Carrera <zachcarrera@gmail.com>
2023-07-24 18:56:54 -0700

    bottle-song: implement Recite

--- bottle-song/bottle_song.go
+++ bottle-song/bottle_song.go
@@ -1,5 +1,10 @@
 package bottlesong
 
+import (
+	"fmt"
+	"strings"
+)
+
 var numbers = []string{
 	"No",
 	"One",
@@ -15,7 +20,23 @@ var numbers = []string{
 }
 
 func Recite(startBottles, takeDown int) []string {
-	panic("Please implement the Recite function")
+	var song []string
+	verse := "%s green bottle%s hanging on the wall,"
+	ending := "There'll be %s green bottle%s hanging on the wall."
+	for i := startBottles; i > startBottles-takeDown; i-- {
+		if i < startBottles {
+			song = append(song, "")
+		}
+		song = append(song, []string{
+			fmt.Sprintf(verse, numbers[i], pluralize(i)),
+			fmt.Sprintf(verse, numbers[i], pluralize(i)),
+			"And if one green bottle should accidentally fall,",
+			fmt.Sprintf(ending, strings.ToLower(numbers[i-1]), pluralize(i-1)),
+		}...)
+	}
+	return song
+}
+
 func pluralize(n int) string {
 	var plural string
 	if n != 1 {
