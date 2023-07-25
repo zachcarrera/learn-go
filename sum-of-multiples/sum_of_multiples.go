1f6c621f155d7d29aa159c3657bb0c1c8800630a
Zach Carrera <zachcarrera@gmail.com>
2023-07-24 21:39:35 -0700

    sum-of-multiples: implement SumMultiples

--- sum-of-multiples/sum_of_multiples.go
+++ sum-of-multiples/sum_of_multiples.go
@@ -1,5 +1,14 @@
 package summultiples
 
 func SumMultiples(limit int, divisors ...int) int {
-	panic("Please implement the SumMultiples function")
+	var sum int
+	for n := 1; n < limit; n++ {
+		for _, v := range divisors {
+			if v != 0 && n%v == 0 {
+				sum += n
+				break
+			}
+		}
+	}
+	return sum
 }
