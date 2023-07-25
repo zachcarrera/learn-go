f881ac6dbe9b10b6e7fc39139c284e64fbfee9a8
Zach Carrera <zachcarrera@gmail.com>
2023-07-24 18:55:07 -0700

    bottle-song: create pluralize function

--- bottle-song/bottle_song.go
+++ bottle-song/bottle_song.go
@@ -2,4 +2,10 @@ package bottlesong
 
 func Recite(startBottles, takeDown int) []string {
 	panic("Please implement the Recite function")
+func pluralize(n int) string {
+	var plural string
+	if n != 1 {
+		plural = "s"
+	}
+	return plural
 }
