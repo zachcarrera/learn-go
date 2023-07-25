2d4b995ae78bf66338f196606e0711eebb5cc080
Zach Carrera <zachcarrera@gmail.com>
2023-07-24 18:56:34 -0700

    bottle-song: create numbers array to map an array index to the number as a string

--- bottle-song/bottle_song.go
+++ bottle-song/bottle_song.go
@@ -1,5 +1,19 @@
 package bottlesong
 
+var numbers = []string{
+	"No",
+	"One",
+	"Two",
+	"Three",
+	"Four",
+	"Five",
+	"Six",
+	"Seven",
+	"Eight",
+	"Nine",
+	"Ten",
+}
+
 func Recite(startBottles, takeDown int) []string {
 	panic("Please implement the Recite function")
 func pluralize(n int) string {
