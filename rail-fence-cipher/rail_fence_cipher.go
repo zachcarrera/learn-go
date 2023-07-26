19ad04b75e0ffb83c0f8c4f1f3518cc02f118e99
Zach Carrera <zachcarrera@gmail.com>
2023-07-25 23:29:41 -0700

    rail-fence-cipher: implement Encode

--- rail-fence-cipher/rail_fence_cipher.go
+++ rail-fence-cipher/rail_fence_cipher.go
@@ -1,7 +1,18 @@
 package railfence
 
+import "strings"
+
 func Encode(message string, rails int) string {
-	panic("Please implement the Encode function")
+	newRails := make([]string, rails)
+	i, sign := 0, 1
+	for _, char := range message {
+		newRails[i] += string(char)
+		if cycle := i + sign; cycle == -1 || cycle == rails {
+			sign *= -1
+		}
+		i += sign
+	}
+	return strings.Join(newRails, "")
 }
 
 func Decode(message string, rails int) string {
