010cf9adf4a1a3a77ae6c88d19174198b309920a
Zach Carrera <zachcarrera@gmail.com>
2023-07-26 11:08:37 -0700

    rail-fence-cipher: create makeRails func that will do the cipher conversion for encode and decode

--- rail-fence-cipher/rail_fence_cipher.go
+++ rail-fence-cipher/rail_fence_cipher.go
@@ -43,3 +43,24 @@ func Decode(message string, rails int) string {
 	}
 	return string(decoded)
 }
+
+// makeRails returns an int slice that represents the index that a rune needs to be placed in a slice to encode or decode a cipher.
+func makeRails(length, numRails int) []int {
+	var rails [][]int
+	rail, dRail := 0, 1
+	for i := 0; i < length; i++ {
+		if i < numRails {
+			rails = append(rails, []int{})
+		}
+		rails[rail] = append(rails[rail], i)
+		if cycle := rail + dRail; cycle == -1 || cycle == numRails {
+			dRail *= -1
+		}
+		rail += dRail
+	}
+	var positions []int
+	for _, slice := range rails {
+		positions = append(positions, slice...)
+	}
+	return positions
+}
