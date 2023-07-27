e8dc24bbae160209a64cfd989c74f60dcc4e8920
Zach Carrera <zachcarrera@gmail.com>
2023-07-26 18:12:23 -0700

    tournament: create teamRecord struct

--- tournament/tournament.go
+++ tournament/tournament.go
@@ -2,6 +2,14 @@ package tournament
 
 import "io"
 
+type teamRecord struct {
+	name   string
+	played int
+	wins   int
+	losses int
+	draws  int
+}
+
 func Tally(reader io.Reader, writer io.Writer) error {
 	panic("Please implement the Tally function")
 }
