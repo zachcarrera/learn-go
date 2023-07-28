8481fa17bb736fb38d417f5c9024f26448f25b1e
Zach Carrera <zachcarrera@gmail.com>
2023-07-28 11:44:52 -0700

    run-length-encoding: implement RunLengthEncode

--- run-length-encoding/run_length_encoding.go
+++ run-length-encoding/run_length_encoding.go
@@ -1,7 +1,28 @@
 package encode
 
+import (
+	"strconv"
+	"strings"
+)
+
 func RunLengthEncode(input string) string {
-	panic("Please implement the RunLengthEncode function")
+	var sb strings.Builder
+	inputChars := []rune(input)
+	for i := 0; i < len(inputChars); i++ {
+		currentChar := inputChars[i]
+		j := i
+		count := 0
+		for j < len(inputChars) && inputChars[j] == currentChar {
+			j++
+			count++
+		}
+		i = j - 1
+		if count > 1 {
+			sb.WriteString(strconv.Itoa(count))
+		}
+		sb.WriteRune(currentChar)
+	}
+	return sb.String()
 }
 
 func RunLengthDecode(input string) string {
