9abb3590c662f327ddfda494724c826164e09570
Zach Carrera <zachcarrera@gmail.com>
2023-08-08 14:28:51 -0700

    phone-number: implement Number

--- phone-number/phone_number.go
+++ phone-number/phone_number.go
@@ -1,7 +1,28 @@
 package phonenumber
 
+import (
+	"errors"
+	"strings"
+	"unicode"
+)
+
 func Number(phoneNumber string) (string, error) {
-	panic("Please implement the Number function")
+	var cleanPhoneNumber strings.Builder
+	for _, char := range phoneNumber {
+		if unicode.IsDigit(char) {
+			cleanPhoneNumber.WriteRune(char)
+		}
+	}
+	cleanedDigits := cleanPhoneNumber.String()
+	if len(cleanedDigits) == 11 && cleanedDigits[0] == '1' {
+		cleanedDigits = cleanedDigits[1:]
+	}
+
+	if cleanedDigits[0] < '2' || cleanedDigits[3] < '2' || len(cleanedDigits) != 10 {
+		return "", errors.New("invalid phone number")
+	}
+
+	return cleanedDigits, nil
 }
 
 func AreaCode(phoneNumber string) (string, error) {
