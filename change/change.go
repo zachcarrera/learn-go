342d42509f17da842b97f21a751ffb37bd63c288
Zach Carrera <zachcarrera@gmail.com>
2023-08-08 16:20:30 -0700

    change: implement Change

--- change/change.go
+++ change/change.go
@@ -1,5 +1,24 @@
 package change
 
+import "errors"
+
 func Change(coins []int, target int) ([]int, error) {
-	panic("Please implement the Change function")
+	if target < 0 {
+		return nil, errors.New("Cannot create negative change")
+	}
+	allPossibleChanges := make([][]int, target+1)
+	allPossibleChanges[0] = []int{}
+	for i := range allPossibleChanges {
+		for _, coin := range coins {
+			if i-coin >= 0 && allPossibleChanges[i-coin] != nil && (allPossibleChanges[i] == nil || len(allPossibleChanges[i-coin])+1 < len(allPossibleChanges[i])) {
+				allPossibleChanges[i] = append([]int{coin}, allPossibleChanges[i-coin]...)
+			}
+		}
+	}
+
+	if allPossibleChanges[target] == nil {
+		return nil, errors.New("Cannot create change")
+	}
+
+	return allPossibleChanges[target], nil
 }
