9a9e22f5a4b92b80a38cf28ecafd5ce2e0f69eca
Zach Carrera <zachcarrera@gmail.com>
2023-08-09 13:53:57 -0700

    grep: create hasFlag function

--- grep/grep.go
+++ grep/grep.go
@@ -3,3 +3,12 @@ package grep
 func Search(pattern string, flags, files []string) []string {
 	panic("Please implement the Search function")
 }
+
+func hasFlag(flags []string, flag string) bool {
+	for _, v := range flags {
+		if v == flag {
+			return true
+		}
+	}
+	return false
+}
