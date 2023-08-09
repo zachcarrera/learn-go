0f31990b4da6be6e37a007a0766de04c6a8db62a
Zach Carrera <zachcarrera@gmail.com>
2023-08-09 14:05:02 -0700

    grep: create buildComparison function and implement it in Search and remove commented out code

--- grep/grep.go
+++ grep/grep.go
@@ -10,37 +10,20 @@ import (
 type lineComparison func(string, string) bool
 
 func Search(pattern string, flags, files []string) []string {
-	// fmt.Printf("pattern: %v\n", pattern)
-	// fmt.Printf("flags: %v\n", flags)
-	// fmt.Printf("files: %v\n", files)
 	var matches []string
+	comparison := buildComparison(flags)
 	for _, fileName := range files {
 		myFile, err := os.Open(fileName)
 		if err != nil {
 			panic("error opening file")
 		}
 		scanner := bufio.NewScanner(myFile)
-		// scanner.Split(bufio.ScanLines)
 		lineNumber := 1
 		for scanner.Scan() {
 			currentLine := scanner.Text()
 			if hasFlag(flags, "-i") {
 				pattern = strings.ToLower(pattern)
 				currentLine = strings.ToLower(currentLine)
-			}
-
-			var comparison lineComparison
-			if hasFlag(flags, "-v") {
-				comparison = func(s1, s2 string) bool { return !strings.Contains(s1, s2) }
-			} else {
-				comparison = strings.Contains
-			}
-			// comparison := strings.Contains
-
-			if hasFlag(flags, "-v") && hasFlag(flags, "-x") {
-				comparison = func(s, substr string) bool { return s != substr }
-			} else if hasFlag(flags, "-x") {
-				comparison = func(s, substr string) bool { return s == substr }
 			}
 
 			if comparison(currentLine, pattern) {
@@ -55,15 +38,28 @@ func Search(pattern string, flags, files []string) []string {
 				if len(files) > 1 {
 					match = fmt.Sprintf("%s:%s", fileName, match)
 				}
-				// fmt.Printf("scanner.Text(): %v\n", scanner.Text())
 				matches = append(matches, match)
 			}
 			lineNumber++
 		}
 		myFile.Close()
 	}
-	// fmt.Printf("matches: %v\n", matches)
 	return matches
+}
+
+func buildComparison(flags []string) lineComparison {
+	hasInvert := hasFlag(flags, "-v")
+	hasMatchEntireLine := hasFlag(flags, "-x")
+	switch {
+	case hasInvert && hasMatchEntireLine:
+		return func(s1, s2 string) bool { return s1 != s2 }
+	case hasMatchEntireLine:
+		return func(s1, s2 string) bool { return s1 == s2 }
+	case hasInvert:
+		return func(s1, s2 string) bool { return !strings.Contains(s1, s2) }
+	default:
+		return strings.Contains
+	}
 }
 
 func hasFlag(flags []string, flag string) bool {
