0811ff951171b9f2b32fa37371967da55ca4d06b
Zach Carrera <zachcarrera@gmail.com>
2023-08-10 13:26:59 -0700

    error-handling: implement Use

--- error-handling/error_handling.go
+++ error-handling/error_handling.go
@@ -1,5 +1,26 @@
 package erratum
 
-func Use(opener ResourceOpener, input string) error {
-	panic("Please implement the Use function")
+func Use(opener ResourceOpener, input string) (err error) {
+	var resource Resource
+	for resource, err = opener(); err != nil; resource, err = opener() {
+		switch err.(type) {
+		case TransientError:
+			continue
+		default:
+			return err
+		}
+	}
+	defer resource.Close()
+
+	defer func() {
+		if e := recover(); e != nil {
+			if f, ok := e.(FrobError); ok {
+				resource.Defrob(f.defrobTag)
+			}
+			err = e.(error)
+		}
+	}()
+
+	resource.Frob(input)
+	return err
 }
