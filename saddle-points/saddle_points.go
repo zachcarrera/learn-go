0c83fdabc0835d5e2d0f0424559919b9408d0ec5
Zach Carrera <zachcarrera@gmail.com>
2023-08-11 16:02:38 -0500

    saddle-points: implement New

--- saddle-points/saddle_points.go
+++ saddle-points/saddle_points.go
@@ -1,12 +1,32 @@
 package matrix
 
+import (
+	"strconv"
+	"strings"
+)
+
 // Define the Matrix and Pair types here.
 type Matrix [][]int
 
 type Pair [2]int
 
 func New(s string) (*Matrix, error) {
-	panic("Please implement the New function")
+	if s == "" {
+		return &Matrix{}, nil
+	}
+	var matrix Matrix
+	for _, line := range strings.Split(s, "\n") {
+		var row []int
+		for _, col := range strings.Split(line, " ") {
+			num, err := strconv.Atoi(col)
+			if err != nil {
+				return nil, err
+			}
+			row = append(row, num)
+		}
+		matrix = append(matrix, row)
+	}
+	return &matrix, nil
 }
 
 func (m *Matrix) Saddle() []Pair {
