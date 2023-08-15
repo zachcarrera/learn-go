96ec6919c2d0340e3b1424bcd45715ed827f88d5
Zach Carrera <zachcarrera@gmail.com>
2023-08-14 23:27:53 -0500

    queen-attack: refactor CanQueenAttack to use regex to validate positions

--- queen-attack/queen_attack.go
+++ queen-attack/queen_attack.go
@@ -1,27 +1,17 @@
 package queenattack
 
-import "errors"
+import (
+	"errors"
+	"regexp"
+)
 
 var errInvalidPosition = errors.New("Invalid queen position")
 
 func CanQueenAttack(whitePosition, blackPosition string) (bool, error) {
 
-	if whitePosition == "" || blackPosition == "" {
-		return false, errInvalidPosition
+	exp := regexp.MustCompile(`^[a-h][1-8]$`)
 
-	}
-	if whitePosition == blackPosition {
-		return false, errInvalidPosition
-	}
-
-	if len(whitePosition) != 2 || len(blackPosition) != 2 {
-		return false, errInvalidPosition
-	}
-
-	if whitePosition[0] < 'a' || whitePosition[0] > 'h' || blackPosition[0] < 'a' || blackPosition[0] > 'h' {
-		return false, errInvalidPosition
-	}
-	if whitePosition[1] < '1' || whitePosition[1] > '8' || blackPosition[1] < '1' || blackPosition[1] > '8' {
+	if !exp.MatchString(whitePosition) || !exp.MatchString(blackPosition) || whitePosition == blackPosition {
 		return false, errInvalidPosition
 	}
 
