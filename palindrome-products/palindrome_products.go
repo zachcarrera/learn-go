7a1d28dc13257cc2bacfba017ef600ac6209eb5a
Zach Carrera <zachcarrera@gmail.com>
2023-08-17 15:27:13 -0700

    palindrome-products: create Product type

--- palindrome-products/palindrome_products.go
+++ palindrome-products/palindrome_products.go
@@ -1,6 +1,10 @@
 package palindrome
 
 // Define Product type here.
+type Product struct {
+	palindrome     int
+	Factorizations [][2]int
+}
 
 func Products(fmin, fmax int) (Product, Product, error) {
 	panic("Please implement the Products function")
