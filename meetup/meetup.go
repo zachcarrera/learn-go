a94f7f30605b0b46a815910c34b3c20c66cf3a06
Zach Carrera <zachcarrera@gmail.com>
2023-08-21 18:42:24 -0700

    meetup: create WeekSchedule type

--- meetup/meetup.go
+++ meetup/meetup.go
@@ -3,6 +3,8 @@ package meetup
 import "time"
 
 // Define the WeekSchedule type here.
+type WeekSchedule int
+
 
 func Day(wSched WeekSchedule, wDay time.Weekday, month time.Month, year int) int {
 	panic("Please implement the Day function")
