8c9fc7521a591345b40a12500fe9ee7105117d74
Zach Carrera <zachcarrera@gmail.com>
2023-08-21 18:43:30 -0700

    meetup: implement Day

--- meetup/meetup.go
+++ meetup/meetup.go
@@ -16,5 +16,39 @@ const (
 )
 
 func Day(wSched WeekSchedule, wDay time.Weekday, month time.Month, year int) int {
-	panic("Please implement the Day function")
+	date := time.Date(year, month, 1, 0, 0, 0, 0, time.Local)
+
+	switch wSched {
+	case First:
+		for wDay != date.Weekday() {
+			date = date.AddDate(0, 0, 1)
+		}
+	case Second:
+		date = date.AddDate(0, 0, 7)
+		for wDay != date.Weekday() {
+			date = date.AddDate(0, 0, 1)
+		}
+	case Third:
+		date = date.AddDate(0, 0, 14)
+		for wDay != date.Weekday() {
+			date = date.AddDate(0, 0, 1)
+		}
+	case Fourth:
+		date = date.AddDate(0, 0, 21)
+		for wDay != date.Weekday() {
+			date = date.AddDate(0, 0, 1)
+		}
+	case Last:
+		date = time.Date(year, month+1, 1, 0, 0, 0, 0, time.Local)
+		date = date.AddDate(0, 0, -1)
+		for wDay != date.Weekday() {
+			date = date.AddDate(0, 0, -1)
+		}
+	case Teenth:
+		date = time.Date(year, month, 13, 0, 0, 0, 0, time.Local)
+		for wDay != date.Weekday() {
+			date = date.AddDate(0, 0, 1)
+		}
+	}
+	return date.Day()
 }
