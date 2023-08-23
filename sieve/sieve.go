13bbb4eaefe3d5dcbf5a04a7a44758cd3d064d64
Zach Carrera <zachcarrera@gmail.com>
2023-08-22 23:25:44 -0700

    sieve: implement Sieve

--- sieve/sieve.go
+++ sieve/sieve.go
@@ -1,5 +1,20 @@
 package sieve
 
 func Sieve(limit int) []int {
-	panic("Please implement the Sieve function")
+	numbersVisited := make([]bool, limit+1)
+	var primes []int
+	for i := 2; i <= limit; i++ {
+		crossedOut := numbersVisited[i]
+		if !crossedOut {
+			primes = append(primes, i)
+			for j := 1; ; j++ {
+				k := i * j
+				if k > limit {
+					break
+				}
+				numbersVisited[k] = true
+			}
+		}
+	}
+	return primes
 }
