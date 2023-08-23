0a1538a85d93fbea83add024da3cca76895bfa7c
Zach Carrera <zachcarrera@gmail.com>
2023-08-23 09:26:35 -0700

    all-your-base: create global error vars

--- all-your-base/all_your_base.go
+++ all-your-base/all_your_base.go
@@ -1,5 +1,13 @@
 package allyourbase
 
+import "errors"
+
+var (
+	errInputTooSmall  = errors.New("input base must be >= 2")
+	errOutputTooSmall = errors.New("output base must be >= 2")
+	errInvalidDigit   = errors.New("all digits must satisfy 0 <= d < input base")
+)
+
 func ConvertToBase(inputBase int, inputDigits []int, outputBase int) ([]int, error) {
 	panic("Please implement the ConvertToBase function")
 }
