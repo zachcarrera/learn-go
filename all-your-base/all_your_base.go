ebdfb63a91dca3a7226fd19f4d14d3332f4679f7
Zach Carrera <zachcarrera@gmail.com>
2023-08-23 09:27:09 -0700

    all-your-base: implement ConvertToBase

--- all-your-base/all_your_base.go
+++ all-your-base/all_your_base.go
@@ -9,5 +9,28 @@ var (
 )
 
 func ConvertToBase(inputBase int, inputDigits []int, outputBase int) ([]int, error) {
-	panic("Please implement the ConvertToBase function")
+	if inputBase < 2 {
+		return nil, errInputTooSmall
+	} else if outputBase < 2 {
+		return nil, errOutputTooSmall
+	}
+
+	var inputToDecimal int
+	for _, digit := range inputDigits {
+		if digit < 0 || digit >= inputBase {
+			return nil, errInvalidDigit
+		}
+		inputToDecimal = inputToDecimal*inputBase + digit
+	}
+
+	if inputToDecimal == 0 {
+		return []int{0}, nil
+	}
+
+	var outputDigits []int
+	for inputToDecimal > 0 {
+		outputDigits = append([]int{inputToDecimal % outputBase}, outputDigits...)
+		inputToDecimal = inputToDecimal / outputBase
+	}
+	return outputDigits, nil
 }
