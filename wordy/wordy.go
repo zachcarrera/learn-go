4484f8c6882eff107587ab5073c467866462621b
Zach Carrera <zachcarrera@gmail.com>
2023-08-25 17:55:24 -0700

    wordy: implement Answer

--- wordy/wordy.go
+++ wordy/wordy.go
@@ -1,5 +1,34 @@
 package wordy
 
+import (
+	"regexp"
+	"strconv"
+)
+
 func Answer(question string) (int, bool) {
-	panic("Please implement the Answer function")
+	if match, _ := regexp.MatchString(`What is -?\d+(?: (?:plus|minus|multiplied by|divided by) -?\d+)*\?`, question); !match {
+		return 0, false
+	}
+
+	operatorsRegex := regexp.MustCompile(`(plus|minus|multiplied by|divided by)`)
+	operators := operatorsRegex.FindAllString(question, -1)
+
+	numbersRegex := regexp.MustCompile(`-?\d+`)
+	numbers := numbersRegex.FindAllString(question, -1)
+
+	sum, _ := strconv.Atoi(numbers[0])
+	for i, operator := range operators {
+		num, _ := strconv.Atoi(numbers[i+1])
+		switch operator {
+		case "plus":
+			sum += num
+		case "minus":
+			sum -= num
+		case "multiplied by":
+			sum *= num
+		case "divided by":
+			sum /= num
+		}
+	}
+	return sum, true
 }
