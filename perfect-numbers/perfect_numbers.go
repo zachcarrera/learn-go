de88145f370de2881e078bcd0c4cb4a20c5cbbf0
Zach Carrera <zachcarrera@gmail.com>
2023-08-28 14:53:00 -0700

    perfect-numbers: create Classification type

--- perfect-numbers/perfect_numbers.go
+++ perfect-numbers/perfect_numbers.go
@@ -1,6 +1,8 @@
 package perfect
 
 // Define the Classification type here.
+type Classification int
+
 
 func Classify(n int64) (Classification, error) {
 	panic("Please implement the Classify function")
