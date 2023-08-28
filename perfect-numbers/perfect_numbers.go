6f0c0a134dd45921ba09bdf693103c5ab0c1d9e3
Zach Carrera <zachcarrera@gmail.com>
2023-08-28 14:53:42 -0700

    perfect-numbers: create Classification enums

--- perfect-numbers/perfect_numbers.go
+++ perfect-numbers/perfect_numbers.go
@@ -3,6 +3,11 @@ package perfect
 // Define the Classification type here.
 type Classification int
 
+const (
+	ClassificationDeficient Classification = 1 + iota
+	ClassificationPerfect
+	ClassificationAbundant
+)
 
 func Classify(n int64) (Classification, error) {
 	panic("Please implement the Classify function")
