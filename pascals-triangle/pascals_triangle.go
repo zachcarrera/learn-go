a9f27bfb6a71fffe302355cb2724a04f3e52b0c9
Zach Carrera <zachcarrera@gmail.com>
2023-08-29 20:45:41 -0700

    pascals-triangle: implement Triangle

--- pascals-triangle/pascals_triangle.go
+++ pascals-triangle/pascals_triangle.go
@@ -1,5 +1,17 @@
 package pascal
 
 func Triangle(n int) [][]int {
-	panic("Please implement the Triangle function")
+	triangle := make([][]int, n)
+	for i := range triangle {
+		triangle[i] = make([]int, i+1)
+		triangle[i][0] = 1
+		triangle[i][i] = 1
+		if i == 0 {
+			continue
+		}
+		for j := range triangle[i][1:i] {
+			triangle[i][j+1] = triangle[i-1][j] + triangle[i-1][j+1]
+		}
+	}
+	return triangle
 }
