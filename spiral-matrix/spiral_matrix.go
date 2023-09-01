22e11f2529282271b218506b2f11f024d37238c1
Zach Carrera <zachcarrera@gmail.com>
2023-09-01 12:34:30 -0700

    spiral-matrix: implement SpiralMatrix

--- spiral-matrix/spiral_matrix.go
+++ spiral-matrix/spiral_matrix.go
@@ -1,5 +1,22 @@
 package spiralmatrix
 
 func SpiralMatrix(size int) [][]int {
-	panic("Please implement the SpiralMatrix function")
+	matrix := make([][]int, size)
+	for i := range matrix {
+		matrix[i] = make([]int, size)
+	}
+	row, col := 0, 0
+	rowDirection, colDirection := 0, 1
+	for i := 1; i <= size*size; i++ {
+		matrix[row][col] = i
+
+		// check when to switch directions, at the bounds of the matrix or when at a location that was already visited
+		if row+rowDirection < 0 || size <= row+rowDirection ||
+			col+colDirection < 0 || size <= col+colDirection ||
+			matrix[row+rowDirection][col+colDirection] != 0 {
+			rowDirection, colDirection = colDirection, -rowDirection
+		}
+		row, col = row+rowDirection, col+colDirection
+	}
+	return matrix
 }
