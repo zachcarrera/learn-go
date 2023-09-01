e3516e92983052996fc6fde23cdfd2d4f52df146
Zach Carrera <zachcarrera@gmail.com>
2023-08-31 19:04:46 -0700

    yacht: create addSameNumber function

--- yacht/yacht.go
+++ yacht/yacht.go
@@ -3,3 +3,13 @@ package yacht
 func Score(dice []int, category string) int {
 	panic("Please implement the Score function")
 }
+
+func addSameNumber(dice []int, num int) int {
+	var sum int
+	for _, die := range dice {
+		if die == num {
+			sum += die
+		}
+	}
+	return sum
+}
