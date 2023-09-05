df54af5cf04b21e19499f7854ceb7633a35452a8
Zach Carrera <zachcarrera@gmail.com>
2023-09-04 21:29:24 -0700

    variable-length-quantity: implement EncodeVarint

--- variable-length-quantity/variable_length_quantity.go
+++ variable-length-quantity/variable_length_quantity.go
@@ -1,7 +1,18 @@
 package variablelengthquantity
 
 func EncodeVarint(input []uint32) []byte {
-	panic("Please implement the EncodeVarint function")
+	var encoded []byte
+	for _, v := range input {
+		// encode the first 7 bits
+		singleEncode := []byte{byte(v % 128)}
+
+		// shift the input number by seven bits and prepend it to singleEncode with the first bit set
+		for v >>= 7; v != 0; v >>= 7 {
+			singleEncode = append([]byte{128 + byte(v%128)}, singleEncode...)
+		}
+		encoded = append(encoded, singleEncode...)
+	}
+	return encoded
 }
 
 func DecodeVarint(input []byte) ([]uint32, error) {
