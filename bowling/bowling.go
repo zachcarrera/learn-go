88ef17483341ef0a49d85523ba20594bdfc134ac
Zach Carrera <zachcarrera@gmail.com>
2023-09-06 20:23:14 -0700

    bowling: implement Roll

--- bowling/bowling.go
+++ bowling/bowling.go
@@ -13,7 +13,29 @@ func NewGame() *Game {
 }
 
 func (g *Game) Roll(pins int) error {
-	panic("Please implement the Roll function")
+	pinsKnockedDown := 0
+
+	if g.isSecondThrow {
+		pinsKnockedDown = g.rolls[len(g.rolls)-1]
+	}
+
+	if pins < 0 || pins+pinsKnockedDown > 10 {
+		return errors.New("Pin count invalid")
+	}
+
+	// get the score and if the game is over return an error
+	if _, err := g.Score(); err == nil {
+		return errors.New("Cannot roll after a completed game")
+	}
+
+	g.rolls = append(g.rolls, pins)
+	g.isSecondThrow = !g.isSecondThrow
+
+	// strike was thrown
+	if pins == 10 {
+		g.isSecondThrow = false
+	}
+	return nil
 }
 
 func (g *Game) Score() (int, error) {
