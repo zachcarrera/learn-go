c81cc237f37ef9f5774a6a33d4ad051118883927
Zach Carrera <zachcarrera@gmail.com>
2023-09-18 16:14:38 -0700

    word-search: create isOutOfBounds function to check if a location is inside a puzzle

--- word-search/word_search.go
+++ word-search/word_search.go
@@ -19,3 +19,7 @@ var directions = []direction{
 func Solve(words []string, puzzle []string) (map[string][2][2]int, error) {
 	panic("Please implement the Solve function")
 }
+
+func isOutOfBounds(x, y int, puzzle []string) bool {
+	return x < 0 || y < 0 || x >= len(puzzle) || y >= len(puzzle[0])
+}
