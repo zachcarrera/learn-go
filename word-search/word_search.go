d82ce7acf63fc1901cf588b0f68a4ab5592b398a
Zach Carrera <zachcarrera@gmail.com>
2023-09-18 16:23:37 -0700

    word-search: create solveWord to find a word in a puzzle

--- word-search/word_search.go
+++ word-search/word_search.go
@@ -1,5 +1,7 @@
 package wordsearch
 
+import "fmt"
+
 type direction struct {
 	dx int
 	dy int
@@ -18,6 +20,29 @@ var directions = []direction{
 
 func Solve(words []string, puzzle []string) (map[string][2][2]int, error) {
 	panic("Please implement the Solve function")
+}
+
+func solveWord(word string, puzzle []string) ([2][2]int, error) {
+	for startX, row := range puzzle {
+		for startY := range row {
+			for _, direction := range directions {
+				completed := true
+				x, y := startX, startY
+				for _, char := range word {
+					if isOutOfBounds(x, y, puzzle) || puzzle[x][y] != byte(char) {
+						completed = false
+						break
+					}
+					x += direction.dx
+					y += direction.dy
+				}
+				if completed {
+					return [2][2]int{{startY, startX}, {y - direction.dy, x - direction.dx}}, nil
+				}
+			}
+		}
+	}
+	return [2][2]int{}, fmt.Errorf("word %s was not found in the puzzle", word)
 }
 
 func isOutOfBounds(x, y int, puzzle []string) bool {
