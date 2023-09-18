767406438e10257055ac31e4d00bf2f5ccd96faa
Zach Carrera <zachcarrera@gmail.com>
2023-09-18 16:07:30 -0700

    word-search: create directions array to represent all the directions a word can arranged in a puzzle

--- word-search/word_search.go
+++ word-search/word_search.go
@@ -5,6 +5,17 @@ type direction struct {
 	dy int
 }
 
+var directions = []direction{
+	{0, 1},
+	{0, -1},
+	{1, 0},
+	{-1, 0},
+	{1, 1},
+	{1, -1},
+	{-1, -1},
+	{-1, 1},
+}
+
 func Solve(words []string, puzzle []string) (map[string][2][2]int, error) {
 	panic("Please implement the Solve function")
 }
