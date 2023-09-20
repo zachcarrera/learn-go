3265c61df25e7459813e8a5f60f9a5e7a8c331c9
Zach Carrera <zachcarrera@gmail.com>
2023-09-19 21:36:58 -0700

    rectangles: create findCorners function to find all the positions in diagram that are corners

--- rectangles/rectangles.go
+++ rectangles/rectangles.go
@@ -5,3 +5,15 @@ type Corner [2]int
 func Count(diagram []string) int {
 	panic("Please implement the Count function")
 }
+
+func findCorners(diagram []string) []Corner {
+	var corners []Corner
+	for y, row := range diagram {
+		for x, char := range row {
+			if char == '+' {
+				corners = append(corners, [2]int{x, y})
+			}
+		}
+	}
+	return corners
+}
