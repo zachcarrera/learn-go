c4fb8c2f08c51500bc29d0ff881b7b91f0428901
Zach Carrera <zachcarrera@gmail.com>
2023-09-28 22:52:39 -0700

    transpose: implement Transpose

--- transpose/transpose.go
+++ transpose/transpose.go
@@ -3,7 +3,19 @@ package transpose
 type matrix [][]rune
 
 func Transpose(input []string) []string {
-	panic("Please implement the Transpose function")
+	var transposed matrix
+	for r, row := range input {
+		for c, char := range row {
+			if len(transposed) <= c {
+				transposed = append(transposed, []rune{})
+			}
+			if len(transposed[c]) < r {
+				transposed[c] = append(transposed[c], setPadding(r-len(transposed[c]), ' ')...)
+			}
+			transposed[c] = append(transposed[c], char)
+		}
+	}
+	return toStringSlice(transposed)
 }
 
 func setPadding(size int, fill rune) []rune {
