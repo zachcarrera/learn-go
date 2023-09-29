a56f380689c5cd28e9f553532c5efa468ddaa28b
Zach Carrera <zachcarrera@gmail.com>
2023-09-28 22:50:45 -0700

    transpose: create setPadding function

--- transpose/transpose.go
+++ transpose/transpose.go
@@ -5,3 +5,11 @@ type matrix [][]rune
 func Transpose(input []string) []string {
 	panic("Please implement the Transpose function")
 }
+
+func setPadding(size int, fill rune) []rune {
+	var padding []rune
+	for i := 0; i < size; i++ {
+		padding = append(padding, fill)
+	}
+	return padding
+}
