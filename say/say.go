b9cd4414aec4954e392f2aa9b1294bd1348df5e9
Zach Carrera <zachcarrera@gmail.com>
2023-10-02 18:36:29 -0700

    say: create places slice

--- say/say.go
+++ say/say.go
@@ -1,5 +1,13 @@
 package say
 
+var places = []string{
+	"",
+	"thousand",
+	"million",
+	"billion",
+	"trillion",
+}
+
 func Say(n int64) (string, bool) {
 	panic("Please implement the Say function")
 }
