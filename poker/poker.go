30f652f48cf227408bb87f46440cfe086d8dd144
Zach Carrera <zachcarrera@gmail.com>
2023-10-04 22:55:53 -0700

    poker: create cardRanks global map to map a string to a CardRank value

--- poker/poker.go
+++ poker/poker.go
@@ -27,6 +27,22 @@ const (
 	Ace
 )
 
+var cardRanks = map[string]CardRank{
+	"2":  Two,
+	"3":  Three,
+	"4":  Four,
+	"5":  Five,
+	"6":  Six,
+	"7":  Seven,
+	"8":  Eight,
+	"9":  Nine,
+	"10": Ten,
+	"J":  Jack,
+	"Q":  Queen,
+	"K":  King,
+	"A":  Ace,
+}
+
 func BestHand(hands []string) ([]string, error) {
 	panic("Please implement the BestHand function")
 }
