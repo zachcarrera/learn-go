a72cc001a50b17fa10088d2e300a06a047915867
Zach Carrera <zachcarrera@gmail.com>
2023-10-04 22:55:22 -0700

    poker: create CardRank type alias and enum values

--- poker/poker.go
+++ poker/poker.go
@@ -9,6 +9,24 @@ const (
 	Club    Suit = '♧'
 )
 
+type CardRank int
+
+const (
+	Two CardRank = iota
+	Three
+	Four
+	Five
+	Six
+	Seven
+	Eight
+	Nine
+	Ten
+	Jack
+	Queen
+	King
+	Ace
+)
+
 func BestHand(hands []string) ([]string, error) {
 	panic("Please implement the BestHand function")
 }
