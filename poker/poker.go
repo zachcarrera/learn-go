7712db6b565b8fc38942daa035d0807457d2565a
Zach Carrera <zachcarrera@gmail.com>
2023-10-04 22:54:58 -0700

    poker: create Suit type alias and constant values

--- poker/poker.go
+++ poker/poker.go
@@ -1,5 +1,14 @@
 package poker
 
+type Suit rune
+
+const (
+	Heart   Suit = '♡'
+	Spade   Suit = '♤'
+	Diamond Suit = '♢'
+	Club    Suit = '♧'
+)
+
 func BestHand(hands []string) ([]string, error) {
 	panic("Please implement the BestHand function")
 }
