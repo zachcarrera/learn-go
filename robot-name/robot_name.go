9a85ef1fbda256b85b026ce0ab61679e0a87571e
Zach Carrera <zachcarrera@gmail.com>
2023-10-09 22:35:31 -0700

    robot-name: create Robot type

--- robot-name/robot_name.go
+++ robot-name/robot_name.go
@@ -1,6 +1,10 @@
 package robotname
 
 // Define the Robot type here.
+type Robot struct {
+	name string
+}
+
 
 func (r *Robot) Name() (string, error) {
 	panic("Please implement the Name function")
