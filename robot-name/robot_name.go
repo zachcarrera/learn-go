27fb471adaf5a0515d95b5366b2b05c49622ac94
Zach Carrera <zachcarrera@gmail.com>
2023-10-09 22:37:08 -0700

    robot-name: implement (*Robot).Name

--- robot-name/robot_name.go
+++ robot-name/robot_name.go
@@ -1,5 +1,11 @@
 package robotname
 
+import (
+	"errors"
+	"math/rand"
+	"strings"
+)
+
 // Define the Robot type here.
 type Robot struct {
 	name string
@@ -8,7 +14,28 @@ type Robot struct {
 var names = map[string]bool{}
 
 func (r *Robot) Name() (string, error) {
-	panic("Please implement the Name function")
+	if r.name != "" {
+		return r.name, nil
+	}
+	if len(names) == 676000 {
+		return "", errors.New("Max number of names reached")
+	}
+
+	var nameBuilder strings.Builder
+	for {
+		nameBuilder.WriteRune(rand.Int31n(26) + 65)
+		nameBuilder.WriteRune(rand.Int31n(26) + 65)
+		nameBuilder.WriteRune(rand.Int31n(10) + 48)
+		nameBuilder.WriteRune(rand.Int31n(10) + 48)
+		nameBuilder.WriteRune(rand.Int31n(10) + 48)
+		if alreadyUsed := names[nameBuilder.String()]; !alreadyUsed {
+			names[nameBuilder.String()] = true
+			break
+		}
+		nameBuilder.Reset()
+	}
+	r.name = nameBuilder.String()
+	return nameBuilder.String(), nil
 }
 
 func (r *Robot) Reset() {
