02b8d263cdb6f89b13bf51c24bbe402bf33ba284
Zach Carrera <zachcarrera@gmail.com>
2023-10-13 16:25:55 -0700

    house: create nouns global slice

--- house/house.go
+++ house/house.go
@@ -1,5 +1,22 @@
 package house
 
+var (
+	nouns = []string{
+		"the house that Jack built.",
+		"the malt",
+		"the rat",
+		"the cat",
+		"the dog",
+		"the cow with the crumpled horn",
+		"the maiden all forlorn",
+		"the man all tattered and torn",
+		"the priest all shaven and shorn",
+		"the rooster that crowed in the morn",
+		"the farmer sowing his corn",
+		"the horse and the hound and the horn",
+	}
+)
+
 func Verse(v int) string {
 	panic("Please implement the Verse function")
 }
