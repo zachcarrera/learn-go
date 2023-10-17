3ba3fae58d208284c043bd894fa3e6d035864705
Zach Carrera <zachcarrera@gmail.com>
2023-10-16 21:30:05 -0700

    ocr-numbers: create digits map

--- ocr-numbers/ocr_numbers.go
+++ ocr-numbers/ocr_numbers.go
@@ -1,5 +1,18 @@
 package ocr
 
+var digits = map[string]string{
+	" _ | ||_|": "0",
+	"     |  |": "1",
+	" _  _||_ ": "2",
+	" _  _| _|": "3",
+	"   |_|  |": "4",
+	" _ |_  _|": "5",
+	" _ |_ |_|": "6",
+	" _   |  |": "7",
+	" _ |_||_|": "8",
+	" _ |_| _|": "9",
+}
+
 func Recognize(string) []string {
 	panic("Please implement the Recognize function")
 }
