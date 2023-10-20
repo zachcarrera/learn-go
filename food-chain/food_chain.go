8c659d3ecc4fdbfde4491f8b6de58f9c221e0fe1
Zach Carrera <zachcarrera@gmail.com>
2023-10-19 19:41:06 -0700

    food-chain: create global slice of type verseInfo

--- food-chain/food_chain.go
+++ food-chain/food_chain.go
@@ -6,6 +6,16 @@ type verseInfo struct {
 	action  string
 }
 
+var animals = []verseInfo{
+	{animal: "fly"},
+	{animal: "spider", comment: "It wriggled and jiggled and tickled inside her.", action: " that wriggled and jiggled and tickled inside her"},
+	{animal: "bird", comment: "How absurd to swallow a bird!"},
+	{animal: "cat", comment: "Imagine that, to swallow a cat!"},
+	{animal: "dog", comment: "What a hog, to swallow a dog!"},
+	{animal: "goat", comment: "Just opened her throat and swallowed a goat!"},
+	{animal: "cow", comment: "I don't know how she swallowed a cow!"},
+}
+
 func Verse(v int) string {
 	panic("Please implement the Verse function")
 }
