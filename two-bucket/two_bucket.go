be318c308e43267dbbf9894d8e5e5078bd27e736
Zach Carrera <zachcarrera@gmail.com>
2023-10-23 21:08:09 -0700

    two-bucket: create bucketOne and bucketTwo consts

--- two-bucket/two_bucket.go
+++ two-bucket/two_bucket.go
@@ -1,5 +1,10 @@
 package twobucket
 
+const (
+	bucketOne = "one"
+	bucketTwo = "two"
+)
+
 func Solve(sizeBucketOne, sizeBucketTwo, goalAmount int, startBucket string) (string, int, int, error) {
 	panic("Please implement the Solve function")
 }
