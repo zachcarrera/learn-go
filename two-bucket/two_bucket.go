9ec4125ca1ee66556fa2c5da8b97e7fa3072b08e
Zach Carrera <zachcarrera@gmail.com>
2023-10-23 21:09:33 -0700

    two-bucket: implement Solve

--- two-bucket/two_bucket.go
+++ two-bucket/two_bucket.go
@@ -1,5 +1,9 @@
 package twobucket
 
+import (
+	"errors"
+)
+
 const (
 	bucketOne = "one"
 	bucketTwo = "two"
@@ -12,7 +16,64 @@ type bucket struct {
 }
 
 func Solve(sizeBucketOne, sizeBucketTwo, goalAmount int, startBucket string) (string, int, int, error) {
-	panic("Please implement the Solve function")
+	if startBucket != bucketOne && startBucket != bucketTwo {
+		return "", 0, 0, errors.New("invalid start bucket")
+	}
+	if sizeBucketOne == 0 || sizeBucketTwo == 0 {
+		return "", 0, 0, errors.New("invalid bucket size")
+	}
+	if goalAmount == 0 || goalAmount > sizeBucketOne && goalAmount > sizeBucketTwo {
+		return "", 0, 0, errors.New("invalid goal amount")
+	}
+
+	bucketsGCD := gcd(sizeBucketOne, sizeBucketTwo)
+	if bucketsGCD > 1 && goalAmount%bucketsGCD != 0 {
+		return "", 0, 0, errors.New("no solution with buckets")
+	}
+
+	var movesMade int
+	var bucketA, bucketB bucket
+
+	// fill the start bucket
+	if startBucket == bucketOne {
+
+		bucketA = bucket{bucketOne, sizeBucketOne, 0}
+		bucketB = bucket{bucketTwo, sizeBucketTwo, 0}
+
+	} else {
+
+		bucketA = bucket{bucketTwo, sizeBucketTwo, 0}
+		bucketB = bucket{bucketOne, sizeBucketOne, 0}
+	}
+
+	for bucketA.currentLevel != goalAmount && bucketB.currentLevel != goalAmount {
+		// break
+		switch {
+		case bucketA.currentLevel == 0:
+			bucketA.currentLevel = bucketA.capacity
+		case bucketB.currentLevel == bucketB.capacity:
+			bucketB.currentLevel = 0
+		case bucketB.currentLevel == 0 && bucketB.capacity == goalAmount:
+			bucketB.currentLevel = bucketB.capacity
+		default:
+			// fill bucket b with bucket a
+			if bucketB.capacity-bucketB.currentLevel >= bucketA.currentLevel {
+				bucketB.currentLevel += bucketA.currentLevel
+				bucketA.currentLevel = 0
+			} else {
+				bucketA.currentLevel = bucketA.currentLevel - (bucketB.capacity - bucketB.currentLevel)
+				bucketB.currentLevel = bucketB.capacity
+			}
+
+		}
+		movesMade++
+	}
+
+	if bucketA.currentLevel == goalAmount {
+		return bucketA.name, movesMade, bucketB.currentLevel, nil
+	}
+
+	return bucketB.name, movesMade, bucketA.currentLevel, nil
 }
 
 func gcd(a, b int) int {
