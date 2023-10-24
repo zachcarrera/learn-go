59108607efb68c3c75b285ffc29ab7d303cc2746
Zach Carrera <zachcarrera@gmail.com>
2023-10-23 21:08:21 -0700

    two-bucket: create bucket struct

--- two-bucket/two_bucket.go
+++ two-bucket/two_bucket.go
@@ -5,6 +5,12 @@ const (
 	bucketTwo = "two"
 )
 
+type bucket struct {
+	name         string
+	capacity     int
+	currentLevel int
+}
+
 func Solve(sizeBucketOne, sizeBucketTwo, goalAmount int, startBucket string) (string, int, int, error) {
 	panic("Please implement the Solve function")
 }
