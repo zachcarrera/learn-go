16127b0fcdef25a7c2fdb003d5487fcc3837b240
Zach Carrera <zachcarrera@gmail.com>
2023-10-23 21:09:12 -0700

    two-bucket: create gcd func to be used to see if two numbers are relative primes

--- two-bucket/two_bucket.go
+++ two-bucket/two_bucket.go
@@ -14,3 +14,10 @@ type bucket struct {
 func Solve(sizeBucketOne, sizeBucketTwo, goalAmount int, startBucket string) (string, int, int, error) {
 	panic("Please implement the Solve function")
 }
+
+func gcd(a, b int) int {
+	if b == 0 {
+		return a
+	}
+	return gcd(b, a%b)
+}
