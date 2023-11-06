64687d41487904e9d0bcaa1d8a5291c1029eb5b4
Zach Carrera <zachcarrera@gmail.com>
2023-11-06 12:33:24 -0800

    pythagorean-triplet: implement Sum

--- pythagorean-triplet/pythagorean_triplet.go
+++ pythagorean-triplet/pythagorean_triplet.go
@@ -10,7 +10,16 @@ func Range(min, max int) []Triplet {
 
 // Sum returns a list of all Pythagorean triplets with a certain perimeter.
 func Sum(p int) []Triplet {
-	panic("Please implement the Sum function")
+	var triplets []Triplet
+	max := p / 2
+	for a := 1; a <= max; a++ {
+		for b := a; b <= max; b++ {
+			if c := p - a - b; isPythagorean(a, b, c) {
+				triplets = append(triplets, Triplet{a, b, c})
+			}
+		}
+	}
+	return triplets
 }
 
 func isPythagorean(a, b, c int) bool {
