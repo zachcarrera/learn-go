25d1dcdff6bdf2baadb0ab90ee8112d93f7a90c0
Zach Carrera <zachcarrera@gmail.com>
2023-11-06 12:33:34 -0800

    pythagorean-triplet: implement Range

--- pythagorean-triplet/pythagorean_triplet.go
+++ pythagorean-triplet/pythagorean_triplet.go
@@ -5,7 +5,17 @@ type Triplet [3]int
 // Range generates list of all Pythagorean triplets with side lengths
 // in the provided range.
 func Range(min, max int) []Triplet {
-	panic("Please implement the Range function")
+	var triplets []Triplet
+	for a := min; a <= max; a++ {
+		for b := a; b <= max; b++ {
+			for c := b; c <= max; c++ {
+				if isPythagorean(a, b, c) {
+					triplets = append(triplets, Triplet{a, b, c})
+				}
+			}
+		}
+	}
+	return triplets
 }
 
 // Sum returns a list of all Pythagorean triplets with a certain perimeter.
