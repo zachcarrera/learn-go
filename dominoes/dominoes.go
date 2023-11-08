b8901af22640e4fe8780a0c6d658a919f5550b3e
Zach Carrera <zachcarrera@gmail.com>
2023-11-07 23:49:48 -0800

    dominoes: implement MakeChain

--- dominoes/dominoes.go
+++ dominoes/dominoes.go
@@ -4,5 +4,33 @@ package dominoes
 type Domino [2]int
 
 func MakeChain(input []Domino) ([]Domino, bool) {
-	panic("Please implement the MakeChain function")
+	if len(input) == 0 || len(input) == 1 && input[0][0] == input[0][1] {
+		return input, true
+	}
+	currentDomino := input[0]
+	for i, domino := range input {
+
+		// skip the first domino
+		if i == 0 {
+			continue
+		}
+
+		// if a match can be made by flipping a domino then flip it
+		if domino[1] == currentDomino[1] {
+			domino[0], domino[1] = domino[1], domino[0]
+		}
+		if domino[0] == currentDomino[1] {
+			// merge the matched pair into one domino
+			// and try to match it with the rest of the dominoes
+			newDomino := [2]int{currentDomino[0], domino[1]}
+			newDominoes := append([]Domino{newDomino}, input[1:i]...)
+			newDominoes = append(newDominoes, input[i+1:]...)
+			oldSolution, ok := MakeChain(newDominoes)
+			if ok {
+				return append([]Domino{currentDomino, domino}, oldSolution[1:]...), true
+			}
+		}
+
+	}
+	return nil, false
 }
